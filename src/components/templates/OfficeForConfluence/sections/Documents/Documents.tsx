a166613b91a430a36547701708ca58328d9658b7
DenisTyan <denistyan16@gmail.com>
2025-02-28 12:43:26 +0500

    feat: add /office-for-confluence page

---
+++ src/components/templates/OfficeForConfluence/sections/Documents/Documents.tsx
@@ -0,0 +1,24 @@
+import { useTranslation } from "next-i18next";
+import { FeatureTabs } from "@src/components/modules/FeatureTabs";
+import { items } from "./data/items";
+
+const Documents = () => {
+  const { t } = useTranslation("office-for-confluence");
+
+  return (
+    <FeatureTabs
+      heading={t("EditAndCoAuthorOfficeDocuments")}
+      headingSize={3}
+      tabletSmallSpacing={["80px", "80px"]}
+      items={items.map(({ label, images }) => ({
+        label: t(label),
+        images: {
+          url: t(images.url),
+          url2x: t(images.url2x),
+        },
+      }))}
+    />
+  );
+};
+
+export { Documents };
