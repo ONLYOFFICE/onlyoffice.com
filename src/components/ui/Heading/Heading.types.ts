c1d8b9cfa882c96e8304bcb26ff7cfd5a298d444
OlimjonovOqil <Okilolimjonov07@gmail.com>
2025-03-26 15:13:23 +0500

    Heading component: fix types

--- src/components/ui/Heading/Heading.types.ts
+++ src/components/ui/Heading/Heading.types.ts
@@ -1,10 +1,12 @@
+import React from "react";
+
 export interface IHeading {
   /** The unique identifier. */
   id?: string;
   /** Additional CSS class names. */
   className?: string;
   /** The text label. If both `children` and `label` are provided, `children` will take precedence. */
-  label?: string;
+  label?: string | React.ReactNode;
   /** Content to be rendered inside. If both `children` and `label` are provided, `children` will take precedence. */
   children?: React.ReactNode;
   /** The color used for styling. */
