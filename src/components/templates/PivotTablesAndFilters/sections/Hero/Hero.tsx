21e1f62c581e4a3e1f750977b6c6ec837a8f7e9a
juli.vytnova <Maystrovich.Julia@onlyoffice.com>
2025-04-15 18:11:44 +0300

    pages: add spreadsheet-editor/pivot-tables-and-filters page

---
+++ src/components/templates/PivotTablesAndFilters/sections/Hero/Hero.tsx
@@ -0,0 +1,54 @@
+import { useTranslation } from "next-i18next";
+import {
+  StyledHero,
+  StyledHeroWrapper,
+  StyledHeroImg,
+  StyledHeroButtons,
+} from "./Hero.styled";
+import { Container } from "@src/components/ui/Container";
+import { Heading } from "@src/components/ui/Heading";
+import { Text } from "@src/components/ui/Text";
+import { Button } from "@src/components/ui/Button";
+
+const Hero = () => {
+  const { t: t1 } = useTranslation("pivot-tables-and-filters");
+  const { t: t2 } = useTranslation("Editors");
+
+  return (
+    <StyledHero
+      desktopSpacing={["120px", "21px"]}
+      tabletSpacing={["112px", "21px"]}
+      tabletSmallSpacing={["96px", "80px"]}
+      mobileSpacing={["96px", "54px"]}
+    >
+      <Container>
+        <StyledHeroWrapper>
+          <Heading level={1} color="#ffffff" label={t1("Header")} />
+          <Text size={1} label={t1("SubHeader")} />
+          <StyledHeroButtons>
+              <Button
+                id="hero-run-on-your-own-server"
+                as="a"
+                href="/download?from=document-editor#docs-enterprise"
+                label={t2("RunOnYourOwnServer")}
+              />
+              <Button
+                id="hero-see-it-in-action"
+                as="a"
+                href="/see-it-in-action"
+                variant="quaternary"
+                label={t2("SeeItInAction")}
+              />
+            </StyledHeroButtons>
+        </StyledHeroWrapper>
+
+        <StyledHeroImg
+          $imgUrl={t1("HeroImgUrl")}
+          $imgUrl2x={t1("HeroImgUrl2x")}
+        />
+      </Container>
+    </StyledHero>
+  );
+};
+
+export { Hero };
