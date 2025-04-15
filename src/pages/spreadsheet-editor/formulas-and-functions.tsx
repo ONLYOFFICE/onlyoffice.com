785b0243c29dbdb4467725b2c858efe5221a2853
juli.vytnova <Maystrovich.Julia@onlyoffice.com>
2025-04-15 18:09:06 +0300

    pages: add spreadsheet-editor/formulas-and-functions page

---
+++ src/pages/spreadsheet-editor/formulas-and-functions.tsx
@@ -0,0 +1,49 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { ILocale } from "@src/types/locale";
+import { Layout } from "@src/components/Layout";
+import { AdventAnnounceBanner } from "@src/components/modules/AdventAnnounceBanner";
+import { Head } from "@src/components/modules/Head";
+import { Header } from "@src/components/modules/Header";
+import { Footer } from "@src/components/modules/Footer";
+import { FormulasAndFunctionsTemplate } from "@src/components/templates/FormulasAndFunctions";
+
+const FormulasAndFunctionsPage = ({ locale }: ILocale) => {
+  const { t } = useTranslation("formulas-and-functions");
+
+  return (
+    <Layout>
+      <Layout.AdventAnnounce>
+        <AdventAnnounceBanner locale={locale} />
+      </Layout.AdventAnnounce>
+      <Layout.Head>
+        <Head title={t("PageTitle")} description={t("PageDescription")} />
+      </Layout.Head>
+      <Layout.Header>
+        <Header locale={locale} />
+      </Layout.Header>
+      <Layout.Main>
+        <FormulasAndFunctionsTemplate />
+      </Layout.Main>
+      <Layout.Footer>
+        <Footer locale={locale} />
+      </Layout.Footer>
+    </Layout>
+  );
+};
+
+export async function getStaticProps({ locale }: ILocale) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        "common",
+        "formulas-and-functions",
+        "GetStarted",
+        "Editors"
+      ])),
+      locale,
+    },
+  };
+}
+
+export default FormulasAndFunctionsPage;
