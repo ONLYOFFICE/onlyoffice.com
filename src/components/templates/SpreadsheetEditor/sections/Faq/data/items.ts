60f75ec1df0dd0b3ae387a9161d72976aae2b7be
juli vytnova <maystrovich.julia@onlyoffice.com>
2025-04-15 15:01:45 +0300

    pages: add spreadsheet-editor

---
+++ src/components/templates/SpreadsheetEditor/sections/Faq/data/items.ts
@@ -0,0 +1,27 @@
+import { ICollapse } from "@src/components/ui/CollapseGroup/Collapse";
+
+interface ICollapseExtended extends ICollapse {
+  links?: { href: string; isExternal?: boolean }[];
+}
+
+export const items: ICollapseExtended[] = [
+  {
+    label: "Q1",
+    children: "A1",
+    links: [
+      { href: "/download-desktop" },
+    ],
+  },
+  {
+    label: "Q2",
+    children: "A2",
+    links: [
+      { href: "/download-desktop" },
+    ],
+  },
+  {
+    label: "Q3",
+    children: "A3",
+    links: [{ href: "/developer-edition" }],
+  },
+];
---
+++ src/components/templates/SpreadsheetEditor/sections/Features/data/items.ts
@@ -0,0 +1,92 @@
+import { IFeatureImageItem } from "@src/components/widgets/FeatureImageItem";
+
+export const items: IFeatureImageItem[] = [
+  {
+    heading: "Feature1Title",
+    text: "Feature1Desc",
+    image: {
+      url: "Feature1ImgUrl",
+      url2x: "Feature1ImgUrl2x",
+      height: 500,
+    },
+  },
+  {
+    heading: "Feature2Title",
+    text: "Feature2Desc",
+    links: [{ label: "LearnMore", href: "/spreadsheet-editor/formulas-and-functions" }],
+    image: {
+      url: "Feature2ImgUrl",
+      url2x: "Feature2ImgUrl2x",
+      height: 500,
+    },
+  },
+  {
+    heading: "Feature3Title",
+    text: "Feature3Desc",
+    links: [{ label: "LearnMore", href: "/spreadsheet-editor/pivot-tables-and-filters" }],
+    image: {
+      url: "Feature3ImgUrl",
+      url2x: "Feature3ImgUrl2x",
+      height: 520,
+    },
+  },
+  {
+    heading: "Feature4Title",
+    text: "Feature4Desc",
+    links: [{ label: "LearnMore", href: "/spreadsheet-editor/charts-and-graphs" }],
+    image: {
+      url: "Feature4ImgUrl",
+      url2x: "Feature4ImgUrl2x",
+      height: 500,
+    },
+  },
+  {
+    heading: "Feature5Title",
+    text: "Feature5Desc",
+    links: [{ label: "LearnMore", href: "/spreadsheet-editor/share-spreadsheet-online" }],
+    image: {
+      url: "Feature5ImgUrl",
+      url2x: "Feature5ImgUrl2x",
+      height: 500,
+    },
+  },
+  {
+    heading: "Feature6Title",
+    text: "Feature6Desc",
+    links: [{ label: "WatchHowItWorks", href: "#" }],
+    image: {
+      url: "Feature6ImgUrl",
+      url2x: "Feature6ImgUrl2x",
+      height: 560,
+    },
+  },
+  {
+    heading: "Feature7Title",
+    text: "Feature7Desc",
+    links: [{ label: "ViewMacroSamples", href: "https://api.onlyoffice.com/plugin/macrosamples/", isExternal: true }],
+    image: {
+      url: "Feature7ImgUrl",
+      url2x: "Feature7ImgUrl2x",
+      height: 520,
+    },
+  },
+  {
+    heading: "Feature8Title",
+    text: "Feature8Desc",
+    image: {
+      url: "Feature8ImgUrl",
+      url2x: "Feature8ImgUrl2x",
+      height: 500,
+    },
+  },
+  {
+    heading: "Feature9Title",
+    text: "Feature9Desc",
+    links: [{ label: "FindAppsYouNeed", href: "/app-directory" }],
+    image: {
+      url: "Feature9ImgUrl",
+      url2x: "Feature9ImgUrl2x",
+      height: 420,
+    },
+  },
+];
