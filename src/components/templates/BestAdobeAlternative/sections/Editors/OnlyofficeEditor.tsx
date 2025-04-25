a672d99898ee150ea11b617b7cf39ad29a8bc6d3
OlimjonovOqil <Okilolimjonov07@gmail.com>
2025-04-25 14:59:04 +0500

    add demo editor

--- src/components/templates/BestAdobeAlternative/sections/Editors/OnlyofficeEditor.tsx
+++ src/components/templates/BestAdobeAlternative/sections/Editors/OnlyofficeEditor.tsx
@@ -1,8 +1,8 @@
 import dynamic from "next/dynamic";
+import { v4 as uuidv4 } from "uuid";
+import { useEffect, useMemo, useState } from "react";
 import { Section } from "@src/components/ui/Section";
 import { Container } from "@src/components/ui/Container";
-import { v4 as uuidv4 } from "uuid";
-import { useMemo } from "react";
 
 const DocumentEditor = dynamic(
   () =>
@@ -13,7 +13,40 @@ const DocumentEditor = dynamic(
 );
 
 export default function OnlyOfficeEditor() {
+  const [token, setToken] = useState("");
   const documentKey = useMemo(() => uuidv4(), []);
+
+  const fileType = "pdf";
+  const title = "Example Form Template Title.pdf";
+  const url = "https://static.onlyoffice.com/assets/docs/samples/oform.pdf";
+
+  const payload = {
+    document: {
+      fileType: fileType,
+      key: documentKey,
+      title: title,
+      url: url,
+      // permissions: {
+      //   edit: true,
+      //   fillForms: false,
+      // },
+    },
+    editorConfig: {
+      mode: "view",
+      // mode: "edit",
+      callbackUrl: "https://example.com/url-to-callback.ashx",
+    },
+  };
+
+  useEffect(() => {
+    fetch("/api/onlyoffice-token", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    })
+      .then((res) => res.json())
+      .then((data) => setToken(data.token));
+  }, []);
 
   return (
     <Section>
@@ -21,20 +54,16 @@ export default function OnlyOfficeEditor() {
         <div style={{ height: "704px", width: "100%" }}>
           <DocumentEditor
             id="pdfEditor"
-            documentServerUrl="https://onlinedocs.onlyoffice.com"
-            // documentServerUrl="https://documentserver/web-apps/apps/api/documents/api.js"
+            documentServerUrl={process.env.ONLYOFFICE_DOCUMENT_SERVER_URL!}
+            // documentServerUrl="https://asc.docs.teamlab.info"
             config={{
+              documentType: fileType,
+              token,
               document: {
-                fileType: "pdf",
-                // key: documentKey,
-                key: "E7FAFC9C22A8",
-                title: "Example Form Template Title.pdf",
-                url: "https://static.onlyoffice.com/assets/docs/samples/oform.pdf",
-              },
-              documentType: "pdf",
-              editorConfig: {
-                mode: "view",
-                callbackUrl: "https://example.com/url-to-callback.ashx",
+                fileType,
+                key: documentKey,
+                title,
+                url,
               },
             }}
           />
