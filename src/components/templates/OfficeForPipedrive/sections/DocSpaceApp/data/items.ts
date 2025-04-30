31b362a8830d9d1676aa52df8c72c81a6f7cc92b
DenisTyan <denistyan16@gmail.com>
2025-04-30 11:53:26 +0500

    feat(office-for-pipedrive): add DocSpaceApp section

---
+++ src/components/templates/OfficeForPipedrive/sections/DocSpaceApp/data/items.ts
@@ -0,0 +1,32 @@
+import { IFeatureSwitcher } from "@src/components/widgets/FeatureSwitcher";
+
+export const items: IFeatureSwitcher["items"] = [
+  {
+    label: "DocspaceAppTab1Label",
+    image: {
+      url: "DocspaceAppTab1ImgUrl",
+      url2x: "DocspaceAppTab1ImgUrl2x",
+    },
+  },
+  {
+    label: "DocspaceAppTab2Label",
+    image: {
+      url: "DocspaceAppTab2ImgUrl",
+      url2x: "DocspaceAppTab2ImgUrl2x",
+    },
+  },
+  {
+    label: "DocspaceAppTab3Label",
+    image: {
+      url: "DocspaceAppTab3ImgUrl",
+      url2x: "DocspaceAppTab3ImgUrl2x",
+    },
+  },
+  {
+    label: "DocspaceAppTab4Label",
+    image: {
+      url: "DocspaceAppTab4ImgUrl",
+      url2x: "DocspaceAppTab4ImgUrl2x",
+    },
+  },
+];
