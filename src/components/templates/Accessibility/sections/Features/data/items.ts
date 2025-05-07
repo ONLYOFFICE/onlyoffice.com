f5952351027ec22c9ab9f92b729be0811364c5e8
juli.vytnova <Maystrovich.Julia@onlyoffice.com>
2025-05-07 13:44:29 +0300

    pages: add accessibility

---
+++ src/components/templates/Accessibility/sections/Features/data/items.ts
@@ -0,0 +1,185 @@
+export const items = [
+    {
+      heading: "VisionTitle",
+      iconPositionX: -4,
+      items: [
+        {
+          heading: "ScreenReaders",
+          items: ["ScreenReadersFeature1", "ScreenReadersFeature2"],
+          image: {
+            url: "ScreenReadersImgUrl",
+            url2x: "ScreenReadersImgUrl2x",
+          },
+          links: [
+            {
+              href: "https://www.onlyoffice.com/blog/2023/10/screen-readers-in-onlyoffice",
+              label: "LearnMore",
+              isExternal: true,
+            },
+          ],
+        },
+        {
+          heading: "UITweaks",
+          items: ["UITweaksFeature1", "UITweaksFeature2", "UITweaksFeature3"],
+          image: {
+            url: "UITweaksImgUrl",
+            url2x: "UITweaksImgUrl2x",
+          },
+          links: [
+            {
+              href: "https://helpcenter.onlyoffice.com/docs/userguides/spreadsheet_editor/accessibility.aspx",
+              label: "LearnMore",
+              isExternal: true,
+            },
+          ],
+        },
+        {
+          heading: "ColorModes",
+          items: ["ColorModesFeature1", "ColorModesFeature2"],
+          image: {
+            url: "ColorModesImgUrl",
+            url2x: "ColorModesImgUrl2x",
+          },
+          links: [
+            {
+              href: "https://helpcenter.onlyoffice.com/docs/userguides/spreadsheet_editor/accessibility.aspx",
+              label: "LearnMore",
+              isExternal: true,
+            },
+          ],
+        },
+        {
+          heading: "MobileWebView",
+          items: ["MobileWebViewFeature1"],
+          image: {
+            url: "MobileWebViewImgUrl",
+            url2x: "MobileWebViewImgUrl2x",
+          },
+          links: [
+            {
+              href: "https://helpcenter.onlyoffice.com/docs/userguides/spreadsheet_editor/accessibility.aspx",
+              label: "LearnMore",
+              isExternal: true,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      heading: "MobilityTitle",
+      iconPositionX: -89,
+      items: [
+        {
+          heading: "Hotkeys",
+          items: ["HotkeysFeature1"],
+          image: {
+            url: "HotkeysImgUrl",
+            url2x: "HotkeysImgUrl2x",
+          },
+          links: [
+            {
+              href: "https://helpcenter.onlyoffice.com/docs/userguides/spreadsheet_editor/accessibility.aspx",
+              label: "LearnMore",
+              isExternal: true,
+            },
+          ],
+        },
+        {
+          heading: "SpeechInput",
+          items: ["SpeechInputFeature1"],
+          image: {
+            url: "SpeechInputImgUrl",
+            url2x: "SpeechInputImgUrl2x",
+          },
+          links: [
+            {
+              href: "https://www.onlyoffice.com/app-directory/speechrecognition",
+              label: "LearnMore",
+              isExternal: true,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      heading: "NeurodiversityTitle",
+      iconPositionX: -177,
+      items: [
+        {
+          heading: "AlternativeText",
+          items: ["AlternativeTextFeature1"],
+          image: {
+            url: "AlternativeTextImgUrl",
+            url2x: "AlternativeTextImgUrl2x",
+          },
+          links: [
+            {
+              href: "https://helpcenter.onlyoffice.com/docs/userguides/spreadsheet_editor/accessibility.aspx",
+              label: "LearnMore",
+              isExternal: true,
+            },
+          ],
+        },
+        {
+          heading: "AutoCorrectFeatures",
+          items: ["AutoCorrectFeaturesFeature1"],
+          image: {
+            url: "AutoCorrectFeaturesImgUrl",
+            url2x: "AutoCorrectFeaturesImgUrl2x",
+          },
+          links: [
+            {
+              href: "https://helpcenter.onlyoffice.com/docs/userguides/spreadsheet_editor/accessibility.aspx",
+              label: "LearnMore",
+              isExternal: true,
+            },
+          ],
+        },
+        {
+          heading: "Speech",
+          items: ["SpeechFeature1"],
+          image: {
+            url: "SpeechImgUrl",
+            url2x: "SpeechImgUrl2x",
+          },
+          links: [
+            {
+              href: "http://www.onlyoffice.com/app-directory/speech",
+              label: "LearnMore",
+              isExternal: true,
+            },
+          ],
+        },
+        {
+          heading: "Typography",
+          items: ["TypographyFeature1"],
+          image: {
+            url: "TypographyImgUrl",
+            url2x: "TypographyImgUrl2x",
+          },
+          links: [
+            {
+              href: "http://www.onlyoffice.com/app-directory/typograf",
+              label: "LearnMore",
+              isExternal: true,
+            },
+          ],
+        },
+        {
+          heading: "Translator",
+          items: ["TranslatorFeature1"],
+          image: {
+            url: "TranslatorImgUrl",
+            url2x: "TranslatorImgUrl2x",
+          },
+          links: [
+            {
+              href: "http://www.onlyoffice.com/app-directory/translator",
+              label: "LearnMore",
+              isExternal: true
+            },
+          ],
+        },
+      ],
+    },
+  ];
