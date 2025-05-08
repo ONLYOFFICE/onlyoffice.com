fba8d7dd06dd915fe56db2639e0d13247f6c0ae8
Hunter-137 <asrarkulovamal@gmail.com>
2025-05-08 16:00:04 +0500

    feat(office-for-ios): add icons and images for Features section, add simple sticky template

--- src/components/templates/OfficeForIos/sections/Features/Features.styled.ts
+++ src/components/templates/OfficeForIos/sections/Features/Features.styled.ts
@@ -3,14 +3,58 @@ import { device } from "@src/utils/device";
 
 const StyledFeaturesWrapper = styled.div`
   display: flex;
+  position: relative;
+  gap: 75px;
 `;
 
-const StyledFeaturesAside = styled.aside``;
+const StyledFeaturesAside = styled.aside`
+  width: 1090px;
+  height: 80vh;
+  box-sizing: content-box;
+  background-color: gray;
+  position: sticky;
+  top: calc(10vh + 36px);
+  left: 0;
+`;
 
-const StyledFeaturesContentList = styled.div``;
+const StyledFeaturesAsideTablet = styled.div`
+  background-image: url("/images/templates/office-for-ios/features/aside_tablet.webp");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-color: blue;
+  width: 100%;
+  height: 80%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10;
+`;
+const StyledFeaturesAsideMobile = styled.div`
+  background-image: url("/images/templates/office-for-ios/features/aside_tablet.webp");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-color: pink;
+  width: 40%;
+  height: 30%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 11;
+`;
+
+const StyledFeaturesContentList = styled.div`
+  & > div {
+    height: 80vh;
+    align-content: center;
+    justify-items: flex-start;
+    text-align: left;
+  }
+`;
 
 export {
   StyledFeaturesWrapper,
   StyledFeaturesAside,
+  StyledFeaturesAsideTablet,
+  StyledFeaturesAsideMobile,
   StyledFeaturesContentList,
 };
--- src/components/templates/OfficeForIos/sections/Features/Features.tsx
+++ src/components/templates/OfficeForIos/sections/Features/Features.tsx
@@ -1,19 +1,69 @@
+import { useTranslation } from "next-i18next";
 import { Section } from "@src/components/ui/Section";
 import { Container } from "@src/components/ui/Container";
+import { FeatureItem } from "@src/components/widgets/FeatureItem";
 
 import {
   StyledFeaturesAside,
+  StyledFeaturesAsideMobile,
+  StyledFeaturesAsideTablet,
   StyledFeaturesContentList,
   StyledFeaturesWrapper,
 } from "./Features.styled";
 
 const Features = () => {
+  const { t } = useTranslation("office-for-ios");
   return (
-    <Section>
-      <Container>
+    <Section background="#e1ffe1" desktopSpacing={["0", "112px"]}>
+      <Container maxWidth="1920">
         <StyledFeaturesWrapper>
-          <StyledFeaturesAside></StyledFeaturesAside>
-          <StyledFeaturesContentList></StyledFeaturesContentList>
+          <StyledFeaturesAside>
+            <StyledFeaturesAsideTablet></StyledFeaturesAsideTablet>
+            <StyledFeaturesAsideMobile></StyledFeaturesAsideMobile>
+          </StyledFeaturesAside>
+          <StyledFeaturesContentList>
+            <FeatureItem
+              maxWidth="280px"
+              icon={{
+                url: "/images/storybook/widgets/feature-item/working.svg",
+                positionX: "left",
+              }}
+              heading="Full-featured text processor"
+              text="Make use of multiple text processing tools : page, paragraph and text formatting, tables, autoshapes, charts, etc. Create text documents of any size and complexity. Print them via AirPrint."
+            />
+            <FeatureItem
+              maxWidth="280px"
+              icon={{
+                url: "/images/storybook/widgets/feature-item/working.svg",
+              }}
+              heading="Heading"
+              text="Working with office files directly within the Box frontend"
+            />
+            <FeatureItem
+              maxWidth="280px"
+              icon={{
+                url: "/images/storybook/widgets/feature-item/working.svg",
+              }}
+              heading="Heading"
+              text="Working with office files directly within the Box frontend"
+            />
+            <FeatureItem
+              maxWidth="280px"
+              icon={{
+                url: "/images/storybook/widgets/feature-item/working.svg",
+              }}
+              heading="Heading"
+              text="Working with office files directly within the Box frontend"
+            />
+            <FeatureItem
+              maxWidth="280px"
+              icon={{
+                url: "/images/storybook/widgets/feature-item/working.svg",
+              }}
+              heading="Heading"
+              text="Working with office files directly within the Box frontend"
+            />
+          </StyledFeaturesContentList>
         </StyledFeaturesWrapper>
       </Container>
     </Section>
