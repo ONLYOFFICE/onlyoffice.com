0d6ecf7fec439140aeb4dc7297fe79944c21b0f9
Hunter-137 <asrarkulovamal@gmail.com>
2025-05-08 18:30:39 +0500

    feat(office-for-ios): add gsap package, add animation for Features section

--- src/components/templates/OfficeForIos/sections/Features/Features.tsx
+++ src/components/templates/OfficeForIos/sections/Features/Features.tsx
@@ -1,3 +1,7 @@
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useTranslation } from "next-i18next";
 import { Section } from "@src/components/ui/Section";
 import { Container } from "@src/components/ui/Container";
@@ -13,60 +17,109 @@ import {
   StyledFeaturesWrapper,
 } from "./Features.styled";
 
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 const Features = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      const items = gsap.utils.toArray(".item");
+      const tabletImages = gsap.utils.toArray(".tabletImage");
+      const mobileImages = gsap.utils.toArray(".mobileImage");
+
+      for (let i = 0; i < items.length - 1; i += 1) {
+        gsap.to(tabletImages[i] as HTMLElement, {
+          opacity: 0,
+          scrollTrigger: {
+            trigger: items[i] as HTMLElement,
+            start: "bottom center+=30%",
+            end: "bottom center-=30%",
+            scrub: true,
+            // markers: true,
+          },
+        });
+
+        gsap.to(mobileImages[i] as HTMLElement, {
+          opacity: 0,
+          scrollTrigger: {
+            trigger: items[i] as HTMLElement,
+            start: "bottom center+=30%",
+            end: "bottom center-=30%",
+            scrub: true,
+            // markers: true,
+          },
+        });
+      }
+    },
+    { scope: containerRef },
+  );
+
   const { t } = useTranslation("office-for-ios");
+
   return (
     <Section background="#FAFAFA" desktopSpacing={["0", "112px"]}>
       <Container maxWidth="1920">
-        <StyledFeaturesWrapper>
+        <StyledFeaturesWrapper ref={containerRef}>
           <StyledFeaturesAside>
             <StyledFeaturesAsideTablet>
               <StyledFeaturesAsideTabletImg
-                $zIndex="1"
+                className="tabletImage"
+                $zIndex="5"
                 $bgUrlTablet="/images/templates/office-for-ios/features/img-tablet_1.webp"
               />
               <StyledFeaturesAsideTabletImg
-                $zIndex="2"
+                className="tabletImage"
+                $zIndex="4"
                 $bgUrlTablet="/images/templates/office-for-ios/features/img-tablet_2.webp"
               />
               <StyledFeaturesAsideTabletImg
+                className="tabletImage"
                 $zIndex="3"
                 $bgUrlTablet="/images/templates/office-for-ios/features/img-tablet_3.webp"
               />
               <StyledFeaturesAsideTabletImg
-                $zIndex="4"
+                className="tabletImage"
+                $zIndex="2"
                 $bgUrlTablet="/images/templates/office-for-ios/features/img-tablet_4.webp"
               />
               <StyledFeaturesAsideTabletImg
-                $zIndex="5"
+                className="tabletImage"
+                $zIndex="1"
                 $bgUrlTablet="/images/templates/office-for-ios/features/img-tablet_5.webp"
               />
             </StyledFeaturesAsideTablet>
             <StyledFeaturesAsideMobile>
               <StyledFeaturesAsideMobileImg
-                $zIndex="1"
+                className="mobileImage"
+                $zIndex="5"
                 $bgUrlMobile="/images/templates/office-for-ios/features/img-mobile_1.webp"
               />
               <StyledFeaturesAsideMobileImg
-                $zIndex="2"
+                className="mobileImage"
+                $zIndex="4"
                 $bgUrlMobile="/images/templates/office-for-ios/features/img-mobile_2.webp"
               />
               <StyledFeaturesAsideMobileImg
+                className="mobileImage"
                 $zIndex="3"
                 $bgUrlMobile="/images/templates/office-for-ios/features/img-mobile_3.webp"
               />
               <StyledFeaturesAsideMobileImg
-                $zIndex="4"
+                className="mobileImage"
+                $zIndex="2"
                 $bgUrlMobile="/images/templates/office-for-ios/features/img-mobile_4.webp"
               />
               <StyledFeaturesAsideMobileImg
-                $zIndex="5"
+                className="mobileImage"
+                $zIndex="1"
                 $bgUrlMobile="/images/templates/office-for-ios/features/img-mobile_5.webp"
               />
             </StyledFeaturesAsideMobile>
           </StyledFeaturesAside>
           <StyledFeaturesContentList>
             <FeatureItem
+              className="item"
               maxWidth="280px"
               icon={{
                 url: "/images/storybook/widgets/feature-item/working.svg",
@@ -76,6 +129,7 @@ const Features = () => {
               text="Make use of multiple text processing tools : page, paragraph and text formatting, tables, autoshapes, charts, etc. Create text documents of any size and complexity. Print them via AirPrint."
             />
             <FeatureItem
+              className="item"
               maxWidth="280px"
               icon={{
                 url: "/images/storybook/widgets/feature-item/working.svg",
@@ -84,6 +138,7 @@ const Features = () => {
               text="Working with office files directly within the Box frontend"
             />
             <FeatureItem
+              className="item"
               maxWidth="280px"
               icon={{
                 url: "/images/storybook/widgets/feature-item/working.svg",
@@ -92,6 +147,7 @@ const Features = () => {
               text="Working with office files directly within the Box frontend"
             />
             <FeatureItem
+              className="item"
               maxWidth="280px"
               icon={{
                 url: "/images/storybook/widgets/feature-item/working.svg",
@@ -100,6 +156,7 @@ const Features = () => {
               text="Working with office files directly within the Box frontend"
             />
             <FeatureItem
+              className="item"
               maxWidth="280px"
               icon={{
                 url: "/images/storybook/widgets/feature-item/working.svg",
