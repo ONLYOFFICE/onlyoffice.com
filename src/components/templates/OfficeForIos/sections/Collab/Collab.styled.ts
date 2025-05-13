0c4358f17cf44f1b356d188d5d72716d709adc79
Hunter-137 <asrarkulovamal@gmail.com>
2025-05-13 12:26:55 +0500

    feat(office-for-ios): add adaptive in Collab section

--- src/components/templates/OfficeForIos/sections/Collab/Collab.styled.ts
+++ src/components/templates/OfficeForIos/sections/Collab/Collab.styled.ts
@@ -7,20 +7,61 @@ const StyledCollabWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   gap: 45px;
+
+  @media ${device.tablet} {
+    flex-direction: column;
+    align-items: center;
+    gap: 0;
+    overflow: hidden;
+  }
 `;
 
 const StyledCollabLeft = styled.div`
   flex-grow: 1;
   justify-items: flex-end;
   margin-top: 120px;
+  padding-left: 16px;
+
+  @media ${device.tablet} {
+    justify-items: center;
+    margin-top: 0;
+    padding: 0 16px;
+  }
 `;
 
 const StyledCollabHeading = styled(Heading)`
   max-width: 270px;
   margin-bottom: 30px;
+
+  @media ${device.tablet} {
+    text-align: center;
+    max-width: 600px;
+  }
+
+  @media ${device.tabletS} {
+    font-size: 32px;
+  }
+
+  @media ${device.mobile} {
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
 `;
 const StyledCollabText = styled(Text)`
   max-width: 270px;
+
+  @media ${device.tablet} {
+    text-align: center;
+    max-width: 600px;
+  }
+
+  @media ${device.mobile} {
+    font-size: 20px;
+  }
+
+  @media ${device.mobileS} {
+    font-size: 14px;
+  }
 `;
 
 const StyledCollabRight = styled.div`
@@ -29,6 +70,17 @@ const StyledCollabRight = styled.div`
   background-image: url("/images/templates/office-for-ios/collab/collab.webp");
   background-size: auto 830px;
   background-repeat: no-repeat;
+
+  @media ${device.tablet} {
+    /* width: 100vw;
+    height: 48.82vw; */
+    width: 108vw;
+    height: 54.82vw;
+    background-size: cover;
+    flex-grow: 1;
+    background-position-x: calc(50% + 6vw);
+    background-position-y: calc(100% + 17vw);
+  }
 `;
 
 export {
--- src/components/templates/OfficeForIos/sections/Collab/Collab.tsx
+++ src/components/templates/OfficeForIos/sections/Collab/Collab.tsx
@@ -13,27 +13,25 @@ const Collab = () => {
   return (
     <Section
       desktopSpacing={["88px", "0"]}
-      tabletSpacing={["88px", "0"]}
-      tabletSmallSpacing={["88px", "0"]}
+      tabletSpacing={["112px", "0"]}
+      tabletSmallSpacing={["112px", "0"]}
       mobileSpacing={["48px", "0"]}
       background="#f5f5f5"
     >
-      <Container maxWidth="1920px">
-        <StyledCollabWrapper>
-          <StyledCollabLeft>
-            <StyledCollabHeading
-              level={2}
-              size={3}
-              label="Real-time collaboration"
-            />
-            <StyledCollabText
-              size={1}
-              label="Be more productive. Connect your app to the ONLYOFFICE cloud. Share docs to co-edit them with your teammates in real time. Comment and track changes. Add graphic annotations."
-            />
-          </StyledCollabLeft>
-          <StyledCollabRight />
-        </StyledCollabWrapper>
-      </Container>
+      <StyledCollabWrapper>
+        <StyledCollabLeft>
+          <StyledCollabHeading
+            level={2}
+            size={3}
+            label="Real-time collaboration"
+          />
+          <StyledCollabText
+            size={1}
+            label="Be more productive. Connect your app to the ONLYOFFICE cloud. Share docs to co-edit them with your teammates in real time. Comment and track changes. Add graphic annotations."
+          />
+        </StyledCollabLeft>
+        <StyledCollabRight />
+      </StyledCollabWrapper>
     </Section>
   );
 };
