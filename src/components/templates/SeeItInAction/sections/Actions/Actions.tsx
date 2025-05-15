e720af0b1550b71fecc23c4df17e872b93a4a22f
Hunter-137 <asrarkulovamal@gmail.com>
2025-05-15 18:54:55 +0500

    feat(see-it-in-action): delete commented code, tryed enable docx document in DocumentEditor in Actions section

--- src/components/templates/SeeItInAction/sections/Actions/Actions.tsx
+++ src/components/templates/SeeItInAction/sections/Actions/Actions.tsx
@@ -17,37 +17,38 @@ const Actions = () => {
   const [token, setToken] = useState("");
   const [config, setConfig] = useState<ITokenResponse["config"] | null>(null);
 
-  console.log("token", token);
-  console.log("config", config);
-
   const mode = "edit";
   const title = "Example Document Title.pdf";
+  const uiTheme = "theme-dark";
   const fileType = "pdf";
   const url = "https://static.onlyoffice.com/assets/docs/samples/oform.pdf";
-  const uiTheme = "theme-dark";
+  // const title = "Example Document Title.docx";
+  // const fileType = "docx";
+  // const url = "https://static.onlyoffice.com/assets/docs/samples/oform.docx";
 
   useEffect(() => {
     const apiUrl = `/api/document-editor?fileType=${fileType}&title=${encodeURIComponent(
       title,
     )}&url=${encodeURIComponent(url)}&mode=${mode}&uiTheme=${uiTheme}`;
 
-    fetch(apiUrl, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((res) => {
+    const fetchApi = async () => {
+      try {
+        const res = await fetch(apiUrl, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+        });
         if (!res.ok) {
           throw new Error(`${res.status}`);
         }
-        return res.json();
-      })
-      .then((data: ITokenResponse) => {
+        const data: ITokenResponse = await res.json();
         setToken(data.token);
         setConfig(data.config);
-      })
-      .catch((err) => {
+      } catch (err) {
         console.error(err);
-      });
+      }
+    };
+
+    fetchApi();
   }, [fileType, title, url, mode, uiTheme]);
 
   const onLoadComponentError = (
@@ -76,34 +77,9 @@ const Actions = () => {
             onLoadComponentError={onLoadComponentError}
           />
         )}
-        <iframe
-          width="100%"
-          height="100%"
-          src="https://site.docs.onlyoffice.com/8.3.3-0d10b80972d36ff5a943a921d724982a/web-apps/apps/pdfeditor/main/index.html?_dc=8.3.3-18&lang=en&customer=ONLYOFFICE&type=desktop&frameEditorId=editorPlaceholder&parentOrigin=https://www.onlyoffice.com&fileType=pdf&isForm=false"
-        />
-        {/* <iframe
-          width="100%"
-          height="100%"
-          src="https://site.docs.onlyoffice.com/8.3.3-0d10b80972d36ff5a943a921d724982a/web-apps/apps/common/index.html?_dc=8.3.3-18&lang=en&customer=ONLYOFFICE&type=desktop&frameEditorId=editorPlaceholder&parentOrigin=https://www.onlyoffice.com&fileType=pdf"
-        /> */}
-        {/* <iframe
-          width="100%"
-          height="100%"
-          src="https://static.onlyoffice.com/assets/docs/samples/oform.pdf"
-        /> */}
       </StyledActionsContent>
     </Container>
   );
 };
 
 export { Actions };
-
-// {
-//   /* <iframe
-//           src="https://site.docs.onlyoffice.com/8.3.3-0d10b80972d36ff5a943a921d724982a/web-apps/apps/documenteditor/main/index.html?_dc=8.3.3-18&lang=en&customer=ONLYOFFICE&type=desktop&frameEditorId=editorPlaceholder&isForm=false&parentOrigin=https://www.onlyoffice.com&fileType=docx"
-//           width="100%"
-//           height="100%"
-//           name="frameEditor"
-//           allow="autoplay; camera; microphone; display-capture; clipboard-write;"
-//         /> */
-// }
