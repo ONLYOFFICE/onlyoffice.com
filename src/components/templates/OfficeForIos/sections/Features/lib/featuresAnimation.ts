7e78822382ae4517c3c8ea22887c1e1cad8e984d
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-05-20 10:48:11 +0500

    fix(office-for-ios): gsap context fixed in featuresAnimation

--- src/components/templates/OfficeForIos/sections/Features/lib/featuresAnimation.ts
+++ src/components/templates/OfficeForIos/sections/Features/lib/featuresAnimation.ts
@@ -6,39 +6,44 @@ const useFeaturesAnimation = (
 ) => {
   useEffect(() => {
     if (typeof window === "undefined") return;
+    let ctx: gsap.Context;
 
       (async () => {
         const { ScrollTrigger } = await import("gsap/ScrollTrigger");
         gsap.registerPlugin(ScrollTrigger);
 
-        const items = gsap.utils.toArray(".item");
-        const tabletImages: HTMLElement[] = gsap.utils.toArray(".tabletImage");
-        const mobileImages: HTMLElement[] = gsap.utils.toArray(".mobileImage");
+        ctx = gsap.context(() => {
+          const items = gsap.utils.toArray(".item");
+          const tabletImages: HTMLElement[] = gsap.utils.toArray(".tabletImage");
+          const mobileImages: HTMLElement[] = gsap.utils.toArray(".mobileImage");
 
-        for (let i = 0; i < items.length - 1; i += 1) {
-          gsap.to(tabletImages[i], {
-            opacity: 0,
-            scrollTrigger: {
-              trigger: items[i] as HTMLElement,
-              start: "bottom center+=30%",
-              end: "bottom center-=30%",
-              scrub: true,
-              // markers: true,
-            },
-          });
+          for (let i = 0; i < items.length - 1; i += 1) {
+            gsap.to(tabletImages[i], {
+              opacity: 0,
+              scrollTrigger: {
+                trigger: items[i] as HTMLElement,
+                start: "bottom center+=30%",
+                end: "bottom center-=30%",
+                scrub: true,
+                // markers: true,
+              },
+            });
 
-          gsap.to(mobileImages[i], {
-            opacity: 0,
-            scrollTrigger: {
-              trigger: items[i] as HTMLElement,
-              start: "bottom center+=30%",
-              end: "bottom center-=30%",
-              scrub: true,
-              // markers: true,
-            },
-          });
-        }
+            gsap.to(mobileImages[i], {
+              opacity: 0,
+              scrollTrigger: {
+                trigger: items[i] as HTMLElement,
+                start: "bottom center+=30%",
+                end: "bottom center-=30%",
+                scrub: true,
+                // markers: true,
+              },
+            });
+          }
+        }, containerRef);
       })();
+
+      return () => ctx?.revert()
     }, [containerRef]);
   };
 
