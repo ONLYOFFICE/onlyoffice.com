f01bfc161fba76df8d3ceb155300533a4dcef3cc
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-05-27 13:55:22 +0500

    feat(customers): add tabs info, add active tab state, change some names in svg/png icons

--- src/components/templates/Customers/sections/Trusted/Trusted.styled.ts
+++ src/components/templates/Customers/sections/Trusted/Trusted.styled.ts
@@ -1,1 +1,36 @@
+import styled from "styled-components";
+import { Heading } from "@src/components/ui/Heading";
 
+const StyledTrustedHeading = styled(Heading)`
+  margin-bottom: 56px;
+`;
+
+const StyledTrustedTabList = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: clamp(16px, 2vw, 32px);
+  border-bottom: 1px solid #e2e2e2;
+  margin-bottom: 56px;
+`;
+
+const StyledTrustedTabItem = styled.span<{
+  $isActive: boolean;
+}>`
+  position: relative;
+  white-space: nowrap;
+  cursor: pointer;
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: -1px;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: ${(props) => (props.$isActive ? "#ff6f3d" : "#e2e2e2")};
+    transition-duration: 300ms;
+  }
+`;
+
+export { StyledTrustedHeading, StyledTrustedTabList, StyledTrustedTabItem };
--- src/components/templates/Customers/sections/Trusted/Trusted.tsx
+++ src/components/templates/Customers/sections/Trusted/Trusted.tsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
 import { Trans, useTranslation } from "next-i18next";
 import { Section } from "@src/components/ui/Section";
 import { Container } from "@src/components/ui/Container";
-import { Heading } from "@src/components/ui/Heading";
 import { Text } from "@src/components/ui/Text";
+import { tabsInfo } from "./data/tabsInfo";
+import { IButtonInfo } from "./Trusted.types";
 
+import { StyledTrustedHeading, StyledTrustedTabItem, StyledTrustedTabList } from "./Trusted.styled";
 
 const Trusted = () => {
   const { t } = useTranslation("customers");
+  const [activeTab, setActiveTab] = useState<IButtonInfo>(tabsInfo[0]);
 
   return (
     <Section>
       <Container>
-        <Heading size={4} level={2} textAlign="center">
+        <StyledTrustedHeading size={4} level={2} textAlign="center">
           <Trans t={t} i18nKey={"TrustedHeadingTrustedAndLove"} components={[
-              <Text as={"span"} color="main" key={0} />
-            ]}
+            <Text as={"span"} color="main" key={0} />
+          ]}
           />
-        </Heading>
+        </StyledTrustedHeading>
+        <StyledTrustedTabList>
+          {tabsInfo.map((tab) => (
+            <StyledTrustedTabItem
+            key={tab.id}
+            $isActive={activeTab.id === tab.id}
+            onClick={() => setActiveTab(tab)}
+            >
+              {tab.title}
+            </StyledTrustedTabItem>
+          ))}
+        </StyledTrustedTabList>
       </Container>
     </Section>
   );
