052758906b627a5dce1613bb4208d97a319f9cb1
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-05-28 16:39:42 +0500

    fix(customers): add Story section (buttons in "footer")

--- src/components/templates/Customers/Customers.tsx
+++ src/components/templates/Customers/Customers.tsx
@@ -2,6 +2,7 @@ import { Hero } from "./sections/Hero";
 import { Trusted } from "./sections/Trusted";
 import { Integration } from "./sections/Integration";
 import { Card } from "./sections/Card";
+import { Story } from "./sections/Story";
 import { ICustomers } from "./Customers.types";
 
 import { StyledCustomersBgWrapper } from "./Customers.styled";
@@ -15,6 +16,7 @@ const CustomersTemplate = ({ customers }: ICustomers) => {
       </StyledCustomersBgWrapper>
       <Integration />
       <Card customers={customers} />
+      <Story />
     </>
   );
 };
---
+++ src/components/templates/Customers/sections/Story/Story.styled.ts
@@ -0,0 +1,11 @@
+import { Container } from "@src/components/ui/Container";
+import styled from "styled-components";
+
+const StyledStoryContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+`;
+
+export { StyledStoryContainer };
