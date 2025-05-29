988c326fbc6e73e87da13d1c6003475dbfba3a9d
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-05-29 10:59:14 +0500

    feat(customers): remove StoryCard component and add own, add adaptive in Card section

--- src/components/templates/Customers/sections/Card/Card.styled.ts
+++ src/components/templates/Customers/sections/Card/Card.styled.ts
@@ -1,29 +1,46 @@
 import styled from "styled-components";
 import { device } from "@src/utils/device";
 import { Button } from "@src/components/ui/Button";
+import { Link } from "@src/components/ui/Link";
+import { Heading } from "@src/components/ui/Heading";
 
-const StyledCardList = styled.div<{
-  $showMore: boolean;
-}>`
+const StyledCardList = styled.div`
   display: grid;
   justify-items: center;
   align-items: center;
   gap: 32px;
   grid-template-columns: repeat(2, 1fr);
 
-  & > div {
-    height: 100%;
-    flex-direction: column;
-    background-color: #fff;
-    border: 1px solid #E5E5E5;
-    box-shadow: 0 7px 25px rgba(0, 0, 0, 0.1);
-    transition: opacity 0.5s ease-in-out, box-shadow 0.3s ease, display 0s linear 0.5s;
-    transition-behavior: allow-discrete;
+  @media ${device.mobile} {
+    grid-template-columns: 1fr;
+  }
 
-    &:hover {
-      box-shadow: 0 20px 40px rgba(0, 0, 0, 0.1);
+  @media ${device.mobileS} {
+    gap: 16px;
+  }
+`;
+
+const StyledCardItem = styled(Link)<{
+  $showMore: boolean;
+}>`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  border: 1px solid #e5e5e5;
+  box-shadow: 0 7px 25px rgba(0, 0, 0, 0.1);
+  transition: opacity 0.5s ease-in-out, box-shadow 0.3s ease, display 0s linear 0.5s;
+  transition-behavior: allow-discrete;
+
+  &:hover {
+    box-shadow: 0 20px 40px rgba(0, 0, 0, 0.1);
+
+    & > h4 {
+      color: #FF6F3D;
     }
+  }
 
+  @media (min-width: 593px) {
     &:nth-child(n + 9) {
       display: ${(props) => (props.$showMore ? "none" : "flex")};
       opacity: ${(props) => (props.$showMore ? "0" : "1")};
@@ -32,24 +49,37 @@ const StyledCardList = styled.div<{
         opacity: 0;
       }
     }
+  }
 
-    & > a {
-      margin-right: 0;
+  @media ${device.mobile} {
+    &:nth-child(n + 7) {
+      display: ${(props) => (props.$showMore ? "none" : "flex")};
+      opacity: ${(props) => (props.$showMore ? "0" : "1")};
 
-      & > div {
-        background-size: contain;
-        background-position: center;
-        background-color: #F9F9F9;
-
-        @media ${device.tablet} {
-          margin: 0 auto;
-        }
+      @starting-style {
+        opacity: 0;
       }
     }
+  }
+`;
 
-    & > h4 {
-      padding: 32px 32px 36px;
-    }
+const StyledCardImg = styled.div<{
+  $imgUrl: string;
+}>`
+  display: block;
+  padding-bottom: 50.287%;
+  background-image: url(${(props) => props.$imgUrl});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+`;
+
+const StyledCardHeading = styled(Heading)`
+  padding: 32px 32px 36px;
+  transition-duration: 300ms;
+
+  @media ${device.mobile} {
+    padding: 25px 20px 36px;
   }
 `;
 
@@ -58,4 +88,4 @@ const StyledCardButton = styled(Button)`
   margin: 48px auto 0;
 `;
 
-export { StyledCardList, StyledCardButton };
+export { StyledCardList, StyledCardItem, StyledCardImg, StyledCardHeading, StyledCardButton };
--- src/components/templates/Customers/sections/Card/Card.tsx
+++ src/components/templates/Customers/sections/Card/Card.tsx
@@ -2,11 +2,10 @@ import { useTranslation } from "next-i18next";
 import { useState } from "react";
 import { Section } from "@src/components/ui/Section";
 import { Container } from "@src/components/ui/Container";
-import { StoryCard } from "@src/components/widgets/StoryCard";
 import { ICustomers } from "../../Customers.types";
 import { useFixUrl } from "./utils/useFixUrl";
 
-import { StyledCardButton, StyledCardList } from "./Card.styled";
+import { StyledCardButton, StyledCardHeading, StyledCardImg, StyledCardItem, StyledCardList } from "./Card.styled";
 
 const Card = ({ customers }: ICustomers) => {
   const { t } = useTranslation("customers");
@@ -16,17 +15,15 @@ const Card = ({ customers }: ICustomers) => {
   return (
     <Section background="#F9F9F9">
       <Container maxWidth="1007px">
-        <StyledCardList $showMore={showMore}>
+        <StyledCardList>
           {fixUrlData.map((item) => (
-            <StoryCard
-              key={item.id}
-              linkUrl={item.url}
-              heading={item.title}
-              imgUrl={item.img?.[0]?.url}
-            />
+            <StyledCardItem key={item.id} href={item.url} target="_blank" $showMore={showMore}>
+              <StyledCardImg $imgUrl={item.img?.[0]?.url} />
+              <StyledCardHeading label={item.title} level={4} size={5} />
+            </StyledCardItem>
           ))}
         </StyledCardList>
-        {fixUrlData.length > 8 && (
+        {fixUrlData.length > 6 && (
           <StyledCardButton
             variant="tertiary"
             borderRadius="3px"
