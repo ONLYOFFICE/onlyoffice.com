ed34c565dc8cc9a18d30cc3c10d5682108f11db5
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-05-29 13:59:00 +0500

    feat(customers): add adaptive in Integration section

--- src/components/templates/Customers/sections/Integration/Integration.styled.ts
+++ src/components/templates/Customers/sections/Integration/Integration.styled.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { device } from "@src/utils/device";
 import { Section } from "@src/components/ui/Section";
 import { Container } from "@src/components/ui/Container";
 import { Heading } from "@src/components/ui/Heading";
@@ -9,25 +10,53 @@ const StyledIntegrationSection = styled(Section)`
   background-repeat: no-repeat;
   background-position: -8vw center;
   background-size: auto 51vw;
+
+  @media ${device.tablet} {
+    background-position: -70vw center;
+    background-size: auto 115vw;
+  }
 `;
 
 const StyledIntegrationContainer = styled(Container)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media ${device.mobile} {
+    flex-direction: column;
+  }
 `;
 
-const StyledIntegrationLeft = styled.div``;
+const StyledIntegrationLeft = styled.div`
+  @media ${device.mobile} {
+    align-self: flex-start;
+  }
+`;
 
 const StyledIntegrationHeading = styled(Heading)`
   max-width: 500px;
   margin-bottom: 17px;
+
+  @media ${device.tabletS} {
+    font-size: 22px;
+    max-width: 416px;
+  }
+
+  @media ${device.mobile} {
+    font-size: 18px;
+  }
 `;
 
 const StyledIntegrationRight = styled.div`
   width: 296px;
   height: 266px;
   position: relative;
+
+  @media ${device.mobile} {
+    width: 136px;
+    height: 122px;
+    align-self: flex-end;
+  }
 `;
 
 const StyledIntegrationIcon = styled.div<{
@@ -57,6 +86,12 @@ const StyledIntegrationIcon = styled.div<{
   &:nth-child(3) {
     bottom: 0;
     right: 0;
+  }
+
+  @media ${device.mobile} {
+    width: 58.87px;
+    height: 58.87px;
+    background-size: 35px 35px;
   }
 `;
 
