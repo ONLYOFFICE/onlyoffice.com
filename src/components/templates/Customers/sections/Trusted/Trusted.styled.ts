e9656e9201a550aa10c8140a0c543d1608fb292c
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-05-29 17:24:02 +0500

    fix(customers): add x-scroll for tabs

--- src/components/templates/Customers/sections/Trusted/Trusted.styled.ts
+++ src/components/templates/Customers/sections/Trusted/Trusted.styled.ts
@@ -9,18 +9,13 @@ const StyledTrustedHeading = styled(Heading)`
 
 const StyledTrustedTabList = styled.ul`
   display: flex;
-  justify-content: center;
   align-items: center;
+  overflow-x: scroll;
   gap: clamp(16px, 2vw, 32px);
   border-bottom: 1px solid #e2e2e2;
   margin-bottom: 56px;
   position: relative;
-
-  @media ${device.desktop} {
-    justify-content: flex-start;
-    overflow-x: scroll;
-    overflow-y: hidden;
-  }
+  scrollbar-width: none;
 `;
 
 const StyledTrustedTabItem = styled.li<{
@@ -34,11 +29,11 @@ const StyledTrustedTabItem = styled.li<{
   &::after {
     content: "";
     position: absolute;
-    bottom: -1px;
+    bottom: 0;
     left: 0;
     width: 100%;
     height: 1px;
-    background-color: ${(props) => (props.$isActive ? "#ff6f3d" : "#e2e2e2")};
+    background-color: ${(props) => (props.$isActive ? "#ff6f3d" : "")};
     transition-duration: 300ms;
   }
 `;
