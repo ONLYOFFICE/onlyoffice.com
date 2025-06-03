dbad7764f3e8ebd29f924343158bde4332ef9f94
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-06-03 15:17:55 +0500

    refactor(find-partners): add spaces in code Partners section

--- src/components/templates/FindPartners/sections/Partners/Partners.tsx
+++ src/components/templates/FindPartners/sections/Partners/Partners.tsx
@@ -95,9 +95,17 @@ const Partners = () => {
 
 
   useEffect(() => {
-    const uniqueKey = new Set(allItems.map((item) => item.name[0].toUpperCase()).sort((a, b) => a.localeCompare(b)));
+    const uniqueKey = new Set(allItems
+      .map((item) => item.name[0]
+      .toUpperCase())
+      .sort((a, b) => a.localeCompare(b))
+    );
     setUniqueKeys([ t("PartnersAll"), ...uniqueKey]);
-    const uniqueCountry = new Set(allItems.map((item) => item.country).sort((a, b) => a.localeCompare(b)));
+
+    const uniqueCountry = new Set(allItems
+      .map((item) => item.country)
+      .sort((a, b) => a.localeCompare(b))
+    );
     setUniqueCountries([...uniqueCountry]);
   }, [allItems, t]);
 
@@ -144,30 +152,62 @@ const Partners = () => {
           ))}
         </StyledPartnersKeyList>
         <StyledPartnersCountryWrapper>
-          <StyledPartnersCountrySelect $isSelectOpen={selectOpen} onClick={() => setSelectOpen(!selectOpen)}>
+          <StyledPartnersCountrySelect
+            $isSelectOpen={selectOpen}
+            onClick={() => setSelectOpen(!selectOpen)}
+          >
             <StyledPartnersCountryInner $isSelectOpen={selectOpen}>
-              <StyledPartnersCountryText $isSelectCountry={selectCountry} $isSelectOpen={selectOpen} label={t("PartnersSelectCountry")} size={2} />
-              <StyledPartnersCountryText $isSelectCountry={selectCountry} $isSelectOpen={selectOpen} label={selectCountry} size={2} />
+              <StyledPartnersCountryText
+                $isSelectCountry={selectCountry}
+                $isSelectOpen={selectOpen}
+                label={t("PartnersSelectCountry")}
+                size={2}
+              />
+              <StyledPartnersCountryText
+                $isSelectCountry={selectCountry}
+                $isSelectOpen={selectOpen}
+                label={selectCountry}
+                size={2}
+              />
             </StyledPartnersCountryInner>
           </StyledPartnersCountrySelect>
           <StyledPartnersCountryOptions $isSelectOpen={selectOpen}>
             {uniqueCountries.map((country) => (
-              <StyledPartnersCountryOption key={country} onClick={() => handleClickOption(country)}>
-                <Text label={country} as={"span"} size={2} color={selectCountry === country ? "#FF6F3D" : undefined} />
+              <StyledPartnersCountryOption
+                key={country}
+                onClick={() => handleClickOption(country)}
+              >
+                <Text
+                  label={country}
+                  as={"span"}
+                  size={2}
+                  color={selectCountry === country ? "#FF6F3D" : undefined}
+                />
               </StyledPartnersCountryOption>
             ))}
           </StyledPartnersCountryOptions>
         </StyledPartnersCountryWrapper>
         <StyledPartnersCardList>
           {items.length > 0 && items.map((item) => (
-            <StyledPartnersCardItem key={item.id} $isItemOpen={itemOpen.includes(item.id)} onClick={() => handleToggleCard(item.id)}>
+            <StyledPartnersCardItem
+              key={item.id}
+              $isItemOpen={itemOpen.includes(item.id)}
+              onClick={() => handleToggleCard(item.id)}
+            >
               <StyledPartnersCardItemLeft>
                 <StyledPartnersCardItemImg $imgUrl={""} />
               </StyledPartnersCardItemLeft>
               <StyledPartnersCardItemRight>
                 <StyledPartnersCardItemHead>
-                  <StyledPartnersCardItemName level={4} size={5} label={item.name ?? ""} />
-                  <StyledPartnersCardItemCountry size={3} label={item.country ?? ""} />
+                  <StyledPartnersCardItemName
+                    level={4}
+                    size={5}
+                    label={item.name ?? ""}
+                  />
+                  <StyledPartnersCardItemCountry
+                    size={3}
+                    label={item.country ?? ""}
+                  />
                   {item.link &&
                     <StyledPartnersCardItemLink
                       $siteText={t("PartnersVisitSite")}
