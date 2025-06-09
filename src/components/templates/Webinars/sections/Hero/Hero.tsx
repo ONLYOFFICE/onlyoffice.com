a251b157fdabe4cdbd3242c3fdf906d817fd7f85
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-06-09 11:32:34 +0500

    feat(webinars): add Hero section

--- src/components/templates/Webinars/sections/Hero/Hero.tsx
+++ src/components/templates/Webinars/sections/Hero/Hero.tsx
@@ -1,14 +1,38 @@
-import { Section } from "@src/components/ui/Section";
+import { Trans, useTranslation } from "next-i18next";
 import { Container } from "@src/components/ui/Container";
-import { Heading } from "@src/components/ui/Heading";
+import { Text } from "@src/components/ui/Text";
+
+import {
+  StyledWebinarsHeading,
+  StyledWebinarsSection,
+  StyledWebinarsText
+} from "./Hero.styled";
 
 const Hero = () => {
+  const { t } = useTranslation("webinars");
+
   return (
-    <Section>
+    <StyledWebinarsSection
+      background="#F6F6F6"
+      desktopSpacing={["112px", "0"]}
+      tabletSpacing={["88px", "0"]}
+      tabletSmallSpacing={["80px", "0"]}
+      mobileSpacing={["48px", "0"]}
+      >
       <Container>
-        <Heading>Hero</Heading>
+        <StyledWebinarsHeading level={1} size={1} textAlign="center">
+          <Trans t={t} i18nKey={"HeroHeadingWatch"} components={[
+              <Text as="span" color="#FF6F3D" key={0} />
+            ]}
+          />
+        </StyledWebinarsHeading>
+        <StyledWebinarsText
+          size={1}
+          textAlign="center"
+          label={t("HeroTextTune")}
+        />
       </Container>
-    </Section>
+    </StyledWebinarsSection>
   );
 };
 
