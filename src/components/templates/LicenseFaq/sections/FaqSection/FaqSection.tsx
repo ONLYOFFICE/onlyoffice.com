8125eab473ea58aa258c5eb9bbe0dd7cc43214c2
juli.vytnova <Maystrovich.Julia@onlyoffice.com>
2025-06-09 16:58:33 +0300

    pages: add license-faq

---
+++ src/components/templates/LicenseFaq/sections/FaqSection/FaqSection.tsx
@@ -0,0 +1,37 @@
+import { useTranslation, Trans } from "next-i18next";
+import { StyledSection, StyledQuestion, StyledAnswer, StyledFaq } from "./FaqSection.styled";
+import { items } from "./data/items";
+import { Link } from "@src/components/ui/Link";
+
+const FaqSection = () => {
+  const { t } = useTranslation("license-faq");
+
+  return (
+    <StyledSection
+      desktopSpacing={["80px", "80px"]}
+      tabletSpacing={["80px", "80px"]}
+      mobileSpacing={["48px", "48px"]}
+    >
+      {items.map((item, idx) => {
+        const linkComponents = item.links.map((link, index) => (
+          <Link color="main" textUnderline href={link.href} key={index} target={link.isExternal ? "_blank" : undefined} />
+        ));
+
+        return (
+          <StyledFaq key={idx}>
+            <StyledQuestion level={2} label={t(item.question)} />
+            <StyledAnswer>
+              <Trans
+                t={t}
+                i18nKey={item.answer}
+                components={linkComponents}
+              />
+            </StyledAnswer>
+          </StyledFaq>
+        );
+      })}
+    </StyledSection>
+  );
+};
+
+export { FaqSection };
---
+++ src/pages/license-faq.tsx
@@ -0,0 +1,46 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { ILocale } from "@src/types/locale";
+import { Layout } from "@src/components/Layout";
+import { AdventAnnounceBanner } from "@src/components/modules/AdventAnnounceBanner";
+import { Head } from "@src/components/modules/head/Head";
+import { Header } from "@src/components/modules/Header";
+import { LicenseFaqTemplate } from "@src/components/templates/LicenseFaq";
+import { Footer } from "@src/components/modules/Footer";
+
+const LicenseFaqPage = ({ locale }: ILocale) => {
+  const { t } = useTranslation("license-faq");
+
+  return (
+    <Layout>
+      <Layout.AdventAnnounce>
+        <AdventAnnounceBanner locale={locale} />
+      </Layout.AdventAnnounce>
+      <Layout.Head>
+        <Head title={t("PageTitle")} description={t("PageDescription")} />
+      </Layout.Head>
+      <Layout.Header>
+        <Header locale={locale} />
+      </Layout.Header>
+      <Layout.Main>
+        <LicenseFaqTemplate />
+      </Layout.Main>
+      <Layout.Footer>
+        <Footer locale={locale} />
+      </Layout.Footer>
+    </Layout>
+  );
+};
+
+export async function getStaticProps({ locale }: ILocale) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        "common",
+        "license-faq",
+      ])),
+      locale,
+    },
+  };
+}
+export default LicenseFaqPage;
