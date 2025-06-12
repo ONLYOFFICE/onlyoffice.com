78b9f35421352e04d05c56861bbac17051450bb6
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-06-12 10:58:56 +0500

    fix(success-stories): pt json translation fixed

--- src/components/templates/SuccessStories/utils/hideCard.ts
+++ src/components/templates/SuccessStories/utils/hideCard.ts
@@ -2,4 +2,5 @@ export const hideCard: Record<string, string[]> = {
   fr: ["rostov-club", "stammtisch", "skatepark-zuidlaren", "icgeurope"],
   de: ["rostov-club", "skatepark-zuidlaren", "icgeurope"],
   es: ["rostov-club", "stammtisch", "skatepark-zuidlaren", "icgeurope"],
+  pt: ["rostov-club", "stammtisch", "skatepark-zuidlaren", "icgeurope"],
 };
