ae89cfd9e2e0eb53c6bb0a0eb645e26a33b4df2d
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-06-19 18:48:04 +0500

    feat(vacancies): add all data items, update translations, add new flag ar

--- src/components/templates/Vacancies/sections/Job/data/items.ts
+++ src/components/templates/Vacancies/sections/Job/data/items.ts
@@ -37,6 +37,132 @@ const items: IJobItem[] = [
     skills: "JobCard2Skills",
     benefits: "JobCard2Benefits",
   },
+  {
+    id: 3,
+    department: {
+      color: "red",
+      label: "JobCardDepartmentSales"
+    },
+    title: "JobCard3Title",
+    location: "JobCardLocationRemote",
+    time: "JobCardTimeFullTime",
+    language: {
+      icon: ["/images/templates/vacancies/job/gb.svg", "/images/templates/vacancies/job/jp.svg"],
+      label: "JobCardLanguageEnJa",
+    },
+    description: "JobCard3Description",
+    requirements: "JobCard3Requirements",
+    skills: "JobCard3Skills",
+    benefits: "JobCard3Benefits",
+  },
+  {
+    id: 4,
+    department: {
+      color: "red",
+      label: "JobCardDepartmentSales"
+    },
+    title: "JobCard4Title",
+    location: "JobCardLocationRemote",
+    time: "JobCardTimeFullTime",
+    language: {
+      icon: ["/images/templates/vacancies/job/gb.svg", "/images/templates/vacancies/job/de.svg"],
+      label: "JobCardLanguageEnDe",
+    },
+    description: "JobCard4Description",
+    requirements: "JobCard4Requirements",
+    skills: "JobCard4Skills",
+    benefits: "JobCard4Benefits",
+  },
+  {
+    id: 5,
+    department: {
+      color: "red",
+      label: "JobCardDepartmentSales"
+    },
+    title: "JobCard5Title",
+    location: "JobCardLocationRemote",
+    time: "JobCardTimeFullTime",
+    language: {
+      icon: ["/images/templates/vacancies/job/gb.svg", "/images/templates/vacancies/job/fr.svg"],
+      label: "JobCardLanguageEnFr",
+    },
+    description: "JobCard5Description",
+    requirements: "JobCard5Requirements",
+    skills: "JobCard5Skills",
+    benefits: "JobCard5Benefits",
+  },
+  {
+    id: 6,
+    department: {
+      color: "red",
+      label: "JobCardDepartmentSales"
+    },
+    title: "JobCard6Title",
+    location: "JobCardLocationRemote",
+    time: "JobCardTimeFullTime",
+    language: {
+      icon: ["/images/templates/vacancies/job/gb.svg", "/images/templates/vacancies/job/jp.svg"],
+      label: "JobCardLanguageEnJa",
+    },
+    description: "JobCard6Description",
+    requirements: "JobCard6Requirements",
+    skills: "JobCard6Skills",
+    benefits: "JobCard6Benefits",
+  },
+  {
+    id: 7,
+    department: {
+      color: "green",
+      label: "JobCardDepartmentCustomerCare"
+    },
+    title: "JobCard7Title",
+    location: "JobCardLocationRemote",
+    time: "JobCardTimeFullTime",
+    language: {
+      icon: ["/images/templates/vacancies/job/gb.svg", "/images/templates/vacancies/job/ar.svg"],
+      label: "JobCardLanguageEnAr",
+    },
+    description: "JobCard7Description",
+    requirements: "JobCard7Requirements",
+    skills: "JobCard7Skills",
+    benefits: "JobCard7Benefits",
+  },
+  {
+    id: 8,
+    department: {
+      color: "blue",
+      label: "JobCardDepartmentMarketing"
+    },
+    title: "JobCard8Title",
+    location: "JobCardLocationRemote",
+    time: "JobCardTimeFullTime",
+    language: {
+      icon: ["/images/templates/vacancies/job/gb.svg", "/images/templates/vacancies/job/sk.svg"],
+      label: "JobCardLanguageEnSr",
+    },
+    description: "JobCard8Description",
+    requirements: "JobCard8Requirements",
+    skills: "JobCard8Skills",
+    benefits: "JobCard8Benefits",
+  },
+  {
+    id: 9,
+    department: {
+      color: "green",
+      label: "JobCardDepartmentCustomerCare"
+    },
+    title: "JobCard9Title",
+    location: "JobCardLocationRemote",
+    time: "JobCardTimeFullTime",
+    language: {
+      icon: ["/images/templates/vacancies/job/gb.svg", "/images/templates/vacancies/job/zh.svg"],
+      label: "JobCardLanguageEnCn",
+    },
+    description: "JobCard9Description",
+    requirements: "JobCard9Requirements",
+    skills: "JobCard9Skills",
+    benefits: "JobCard9Benefits",
+  },
 ];
 
 export { items };
