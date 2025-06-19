90cc82e6a991e3068d7b721f9df290bac61b29be
DenisTyan <denistyan16@gmail.com>
2025-06-19 14:27:17 +0500

    feat: remove pages for test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,14 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { ILocale } from "@src/types/locale";
 import { Layout } from "@src/components/Layout";
-import { AdventAnnounceBanner } from "@src/components/modules/AdventAnnounceBanner";
 import { Head } from "@src/components/modules/head/Head";
-import { Header } from "@src/components/modules/Header";
 import { MainTemplate } from "@src/components/templates/Main";
-import { Footer } from "@src/components/modules/Footer";
 
 const MainPage = ({ locale }: ILocale) => {
   const { t } = useTranslation("main");
 
   return (
     <Layout>
-      <Layout.AdventAnnounce>
-        <AdventAnnounceBanner locale={locale} />
-      </Layout.AdventAnnounce>
       <Layout.Head>
         <Head
           title={t("PageTitle")}
@@ -23,15 +17,9 @@ const MainPage = ({ locale }: ILocale) => {
           titleOG={t("PageTitleOG")}
         />
       </Layout.Head>
-      <Layout.Header>
-        <Header locale={locale} />
-      </Layout.Header>
       <Layout.Main>
         <MainTemplate locale={locale} />
       </Layout.Main>
-      <Layout.Footer>
-        <Footer locale={locale} />
-      </Layout.Footer>
     </Layout>
   );
 };
