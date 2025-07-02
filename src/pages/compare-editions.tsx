d039c2096ff3bc3796a1ce05707eeb7d1f86d6b7
juli.vytnova <Maystrovich.Julia@onlyoffice.com>
2025-07-02 19:35:47 +0300

    pages: add compare-editions

---
+++ src/pages/compare-editions.tsx
@@ -0,0 +1,44 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { ILocale } from "@src/types/locale";
+import { Layout } from "@src/components/Layout";
+import { AdventAnnounceBanner } from "@src/components/modules/AdventAnnounceBanner";
+import { Head } from "@src/components/modules/head/Head";
+import { Header } from "@src/components/modules/Header";
+import { CompareEditionsTemplate } from "@src/components/templates/CompareEditions";
+import { Footer } from "@src/components/modules/Footer";
+
+const CompareEditionsPage = ({ locale }: ILocale) => {
+  const { t } = useTranslation("compare-editions");
+  
+  return (
+    <Layout>
+      <Layout.AdventAnnounce>
+        <AdventAnnounceBanner locale={locale} />
+      </Layout.AdventAnnounce>
+      <Layout.Head>
+        <Head title={t("PageTitle")} description={t("PageDescription")} />
+      </Layout.Head>
+      <Layout.Header>
+        <Header locale={locale}/>
+      </Layout.Header>
+      <Layout.Main>
+        <CompareEditionsTemplate />
+      </Layout.Main>
+      <Layout.Footer>
+        <Footer locale={locale} />
+      </Layout.Footer>
+    </Layout>
+  );
+};
+
+export async function getStaticProps({ locale }: ILocale) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "compare-editions"])),
+      locale,
+    },
+  };
+}
+
+export default CompareEditionsPage; 
