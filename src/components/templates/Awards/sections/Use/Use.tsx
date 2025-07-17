eaf299b7e546e2d23671a0446057e875ecbcb6fb
Svetlana Lisina <svetik_lisik@mail.ru>
2025-07-17 13:38:42 +0300

    pages: add /awards

---
+++ src/components/templates/Awards/sections/Use/Use.tsx
@@ -0,0 +1,43 @@
+import {
+  StyledUse,
+  StyledUseHeading,
+} from "./Use.styled";
+import { Section } from "@src/components/ui/Section";
+import { Container } from "@src/components/ui/Container";
+import { Button } from "@src/components/ui/Button";
+import { Trans, useTranslation } from "next-i18next";
+import { Text } from "@src/components/ui/Text";
+
+const Use = () => {
+  const { t } = useTranslation("awards");
+
+  return (
+    <Section
+      desktopSpacing={["80px", "80px"]}
+      tabletSpacing={["56px", "56px"]}
+      tabletSmallSpacing={["56px", "56px"]}
+      background="#f5f5f5"
+    >
+      <Container>
+      <StyledUse>
+          <StyledUseHeading level={1} size={5} textAlign="right">
+              <Trans
+                t={t}
+                i18nKey={"UseTitle"}
+                components={[
+                  <Text as="span" color="main" key={0} />
+                ]}
+              />
+            </StyledUseHeading>
+            <Button
+              as="a"
+              label={t("UseButton")}
+              href="/registration?from=awards"
+            />
+      </StyledUse>
+      </Container>
+    </Section>
+  );
+};
+
+export { Use };
