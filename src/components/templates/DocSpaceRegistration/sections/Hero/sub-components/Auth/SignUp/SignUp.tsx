0310dc3350e227bdee4ac11a4242149f142598b2
DenisTyan <denistyan16@gmail.com>
2025-07-22 18:32:18 +0500

    feat(docspace-registration): add handleGoogleSignIn for SignUp

--- src/components/templates/DocSpaceRegistration/sections/Hero/sub-components/Auth/SignUp/SignUp.tsx
+++ src/components/templates/DocSpaceRegistration/sections/Hero/sub-components/Auth/SignUp/SignUp.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useTranslation, Trans } from "next-i18next";
 import ReactCaptcha from "@hcaptcha/react-hcaptcha";
 import {
@@ -23,9 +23,17 @@ import { Button } from "@src/components/ui/Button";
 import { HCaptcha } from "@src/components/ui/HCaptcha";
 import { validateEmail } from "@src/utils/validators";
 
+declare global {
+  interface Window {
+    SignInByGoogle: (data: string) => void;
+  }
+}
+
 const SignUp = ({ setEmail }: ISignUp) => {
   const { t } = useTranslation("docspace-registration");
   const hCaptchaRef = useRef<ReactCaptcha | null>(null);
+  const modalDialog = useRef<Window | null>(null);
+  const intervalId = useRef<NodeJS.Timeout | null>(null);
 
   const [formData, setFormData] = useState({
     email: "",
@@ -110,6 +118,58 @@ const SignUp = ({ setEmail }: ISignUp) => {
     setisFormLoading(false);
   };
 
+  const checkSignInCode = () => {
+    const code = localStorage.getItem("code");
+    if (code) {
+      localStorage.removeItem("code");
+      if (intervalId.current) clearInterval(intervalId.current);
+
+      const token = window.btoa(
+        JSON.stringify({
+          auth: "google",
+          mode: "popup",
+          callback: "SignInByGoogle",
+        }),
+      );
+
+      if (modalDialog.current) {
+        modalDialog.current.location.href = `${location.href}/login?p=${token}&code=${code}`;
+      }
+    }
+  };
+
+  const handleGoogleSignIn = () => {
+    if (intervalId.current) clearInterval(intervalId.current);
+
+    modalDialog.current = window.open(
+      `${location.href}/login?auth=google&mode=popup&callback=SignInByGoogle`,
+      "signup",
+      "width=800,height=500,status=no,toolbar=no,menubar=no,resizable=yes,scrollbars=no",
+    );
+
+    intervalId.current = setInterval(checkSignInCode, 500);
+  };
+
+  useEffect(() => {
+    window.SignInByGoogle = (data) => {
+      try {
+        console.log("Callback received:", JSON.stringify(data));
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("Unexpected error:", error.message);
+        } else {
+          console.error("Unexpected error:", error);
+        }
+      }
+    };
+
+    return () => {
+      if (intervalId.current) {
+        clearInterval(intervalId.current);
+      }
+    };
+  }, []);
+
   return (
     <>
       <StyledSignUpAccount>
@@ -140,7 +200,7 @@ const SignUp = ({ setEmail }: ISignUp) => {
         </StyledSignUpHeader>
 
         <StyledSignUpGoogleButton
-          // onClick={openGooglePopup}
+          onClick={handleGoogleSignIn}
           variant="tertiary"
           size="small"
           fullWidth
