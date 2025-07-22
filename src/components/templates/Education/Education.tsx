25db7131d683bc13243323fb4436e1ff4299c2b6
OlimjonovOqil <okilolimjonov07@gmail.com>
2025-07-22 22:03:35 +0500

    restore: recover complete Education template with all components, data, images and translations

---
+++ src/components/templates/Education/Education.tsx
@@ -0,0 +1,23 @@
+import { Benefits } from "./sections/Benefits";
+import { Contact } from "./sections/Contact";
+import { Hero } from "./sections/Hero";
+import { Marketplace } from "./sections/Marketplace";
+import { Quote } from "./sections/Quote";
+import { UserTypes } from "./sections/UserTypes";
+import { Partners } from "./sections/Partners";
+
+const EducationTemplate = () => {
+  return (
+    <>
+      <Hero />
+      <Partners />
+      <UserTypes />
+      <Quote />
+      <Benefits />
+      <Marketplace />
+      <Contact />
+    </>
+  );
+};
+
+export { EducationTemplate };
