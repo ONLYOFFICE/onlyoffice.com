e43c31bbf0988cdc7e63ab6509fa17058498f0b7
Vyacheslav Belov <vyacheslav.belov@onlyoffice.com>
2025-07-24 11:18:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,35 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*.aspx',
+        destination: '/:path*',
+        permanent: true,
+      },
+      {
+        source: '/app-server',
+        destination: '/docspace',
+        permanent: true,
+      },
+      {
+        source: '/cloud-office',
+        destination: '/docspace',
+        permanent: true,
+      },
+      {
+        source: '/free-alternative-to-microsoft-office',
+        destination: '/desktop',
+        permanent: true,
+      },
+      {
+        source: '/survey-workspace',
+        destination: '/docspace',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
