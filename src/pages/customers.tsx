3f9342eeed353682c30642b29da148b6975063ba
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-07-25 13:32:30 +0500

    feat: highlight for Header, update actual translations from prod

--- src/pages/customers.tsx
+++ src/pages/customers.tsx
@@ -2,7 +2,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { ILocale } from "@src/types/locale";
 import { Layout } from "@src/components/Layout";
-import { AdventAnnounceBanner } from "@src/components/modules/AdventAnnounceBanner";
+import { AdventAnnounce } from "@src/components/modules/AdventAnnounce";
 import { Head } from "@src/components/modules/head/Head";
 import { Header } from "@src/components/modules/Header";
 import { CustomersTemplate } from "@src/components/templates/Customers";
@@ -16,13 +16,19 @@ const CustomersPage = ({ locale, customers }: ILocale & ICustomers) => {
   return (
     <Layout>
       <Layout.AdventAnnounce>
-        <AdventAnnounceBanner locale={locale} />
+        <AdventAnnounce locale={locale} />
       </Layout.AdventAnnounce>
       <Layout.Head>
         <Head title={t("PageTitle")} titleOG={t("PageTitleOG")} description={t("PageDescription")} />
       </Layout.Head>
       <Layout.Header>
-        <Header locale={locale} />
+        <Header
+          locale={locale}
+          highlight={{
+            buttonId: "oo-menu-item-btn-resources",
+            linkId: "oo-menu-link-customers",
+          }}
+        />
       </Layout.Header>
       <Layout.Main>
         <CustomersTemplate customers={customers} />
