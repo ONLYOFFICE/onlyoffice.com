ab7a3fb7dbf26c3526f16f3b3d101d6c4a36c4e0
DenisTyan <denistyan16@gmail.com>
2025-07-30 13:37:37 +0500

    feat: add Select component

---
+++ src/components/ui/Select/Select.tsx
@@ -0,0 +1,173 @@
+import { useState, useRef, useEffect } from "react";
+import { useTranslation } from "next-i18next";
+import {
+  StyledSelect,
+  StyledSelectInput,
+  StyledSelectInputValue,
+  StyledSelectInputCount,
+  StyledSelectInputIcon,
+  StyledSelectCaption,
+  StyledSelectLabel,
+  StyledSelectOptions,
+  StyledSelectOption,
+} from "./Select.styled";
+import { ISelect, ISelectOption } from "./Select.types";
+
+const Select = ({
+  selected,
+  setSelected,
+  options,
+  label,
+  labelVariant = "primary",
+  placeholder,
+  disabled,
+  required,
+  multiple,
+  multipleCount,
+  status = "default",
+  caption,
+  maxWidth = "472px",
+  withReset,
+  withSelectedIcon,
+}: ISelect) => {
+  const { t } = useTranslation("Select");
+  const selectRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef(null);
+  const optionsRef = useRef<HTMLDivElement>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        selectRef.current &&
+        !selectRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleOption = (option: ISelectOption) => {
+    if (multiple) {
+      if (selected.find((s) => s.value === option.value)) {
+        setSelected((prev) => prev.filter((s) => s.value !== option.value));
+      } else {
+        setSelected((prev) => [...prev, option]);
+      }
+    } else {
+      setSelected([option]);
+    }
+
+    setIsOpen(false);
+  };
+
+  const resetSelection = () => setSelected([]);
+
+  const isSelected = (option: ISelectOption) =>
+    selected.find((s) => s.value === option.value);
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Tab") {
+      const focusableItems = optionsRef.current?.querySelectorAll("button");
+      const firstElement = focusableItems?.[0];
+      const lastElement = focusableItems?.[focusableItems.length - 1];
+
+      if (document.activeElement === lastElement && !e.shiftKey) {
+        setIsOpen(false);
+      } else if (document.activeElement === firstElement && e.shiftKey) {
+        setIsOpen(false);
+      }
+
+      if (document.activeElement === inputRef.current && e.shiftKey) {
+        setIsOpen(false);
+      }
+    }
+  };
+
+  return (
+    <StyledSelect
+      ref={selectRef}
+      onKeyDown={handleKeyDown}
+      $maxWidth={maxWidth}
+    >
+      <StyledSelectInput
+        ref={inputRef}
+        onClick={() => !disabled && setIsOpen((prev) => !prev)}
+        $isOpen={isOpen}
+        $placeholder={selected.length === 0 && placeholder ? true : false}
+        $status={status}
+        $labelVariant={labelVariant}
+        disabled={disabled}
+      >
+        {label && (
+          <StyledSelectLabel
+            $isOpen={isOpen}
+            $selected={selected.length > 0}
+            $status={status}
+            $labelVariant={labelVariant}
+          >
+            {label}
+            {required && <span>*</span>}
+          </StyledSelectLabel>
+        )}
+        <StyledSelectInputValue>
+          {selected.length === 0 && isOpen
+            ? placeholder
+            : multiple
+              ? selected.map((s) => s.label).join(", ")
+              : selected[0]?.label}
+        </StyledSelectInputValue>
+
+        {multipleCount && selected.length > 0 && (
+          <StyledSelectInputCount>({selected.length})</StyledSelectInputCount>
+        )}
+        <StyledSelectInputIcon
+          $isOpen={isOpen}
+          $selected={selected.length > 0}
+          $status={status}
+        />
+      </StyledSelectInput>
+
+      {caption && <StyledSelectCaption>{caption}</StyledSelectCaption>}
+
+      {isOpen && (
+        <StyledSelectOptions ref={optionsRef}>
+          {withReset && (
+            <StyledSelectOption
+              onClick={(e) => {
+                e.stopPropagation();
+                resetSelection();
+                setIsOpen(false);
+              }}
+              $isReset
+            >
+              {t("Reset")}
+            </StyledSelectOption>
+          )}
+
+          {options.map((option) => (
+            <StyledSelectOption
+              $isSelected={isSelected(option) ? true : false}
+              $withSelectedIcon={withSelectedIcon}
+              key={option.value}
+              onClick={(e) => {
+                e.stopPropagation();
+                toggleOption(option);
+              }}
+            >
+              {option.label}
+            </StyledSelectOption>
+          ))}
+        </StyledSelectOptions>
+      )}
+    </StyledSelect>
+  );
+};
+
+export { Select };
---
+++ src/components/ui/Select/Select.types.ts
@@ -0,0 +1,39 @@
+export interface ISelectOption {
+  /** Display label shown in the dropdown */
+  label: string;
+  /** Internal value associated with the option */
+  value: string;
+}
+
+export interface ISelect {
+  /** Currently selected options */
+  selected: ISelectOption[];
+  /** Function to update the selected options */
+  setSelected: React.Dispatch<React.SetStateAction<ISelectOption[]>>;
+  /** List of available options to select from */
+  options: ISelectOption[];
+  /** Label displayed above the select field */
+  label?: string;
+  /** Variant of the label */
+  labelVariant?: "primary" | "secondary";
+  /** Placeholder text displayed inside the input field */
+  placeholder?: string;
+  /** Disables interaction with the select component */
+  disabled?: boolean;
+  /** Displays an asterisk (*) to indicate the field is required */
+  required?: boolean;
+  /** Enables multiple option selection */
+  multiple?: boolean;
+  /** Shows the number of selected options (e.g., "(3)") */
+  multipleCount?: boolean;
+  /** Defines the status of the input field (default, success, or error) */
+  status?: "default" | "success" | "error";
+  /** Caption text displayed below the input field for error messages */
+  caption?: string;
+  /** Maximum width of the select component (e.g., "400px", "100%") */
+  maxWidth?: string;
+  /** Shows a "Reset" option to clear the selection */
+  withReset?: boolean;
+  /** Shows an icon next to the selected option */
+  withSelectedIcon?: boolean;
+}
