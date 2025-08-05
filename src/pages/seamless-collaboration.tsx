4effd42b1b66d8c5c2c14c1e07f89ebc2dbcc6f2
Irina Tiulneva <irina.malysheva@onlyoffice.com>
2025-08-05 18:41:19 +0300

    /seamless-collaboration: init + hero.

---
+++ src/pages/seamless-collaboration.tsx
@@ -0,0 +1,55 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { ILocale } from "@src/types/locale";
+import { Layout } from "@src/components/Layout";
+import { AdventAnnounce } from "@src/components/modules/AdventAnnounce";
+import { Head } from "@src/components/modules/head/Head";
+import { Header } from "@src/components/modules/Header";
+import { Footer } from "@src/components/modules/Footer";
+import { SeamlessCollaborationTemplate } from "@src/components/templates/SeamlessCollaboration";
+
+const SeamlessCollaborationPage = ({ locale }: ILocale) => {
+  const { t } = useTranslation("seamless-collaboration");
+
+  return (
+    <Layout>
+      <Layout.AdventAnnounce>
+        <AdventAnnounce locale={locale} />
+      </Layout.AdventAnnounce>
+      <Layout.Head>
+        <Head title={t("PageTitle")} description={t("PageDescription")} />
+      </Layout.Head>
+      <Layout.Header>
+        <Header
+          locale={locale}
+          highlight={{
+            buttonId: "oo-menu-item-btn-products",
+            linkId: "oo-menu-link-docs-e-book-creator",
+          }}
+        />
+      </Layout.Header>
+      <Layout.Main>
+        <SeamlessCollaborationTemplate />
+      </Layout.Main>
+      <Layout.Footer>
+        <Footer locale={locale} />
+      </Layout.Footer>
+    </Layout>
+  );
+};
+
+export async function getStaticProps({ locale }: ILocale) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        "common",
+        "seamless-collaboration",
+        "GetStarted",
+        "Editors",
+      ])),
+      locale,
+    },
+  };
+}
+
+export default SeamlessCollaborationPage;
