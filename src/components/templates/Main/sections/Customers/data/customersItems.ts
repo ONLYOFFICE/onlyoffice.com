034429bd2b18bc9a9fbe43679f9f7c715a96b7a8
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-08-14 18:51:29 +0500

    feat: update customersLogoItems, update their types, add fr locale logo

--- src/components/templates/Main/sections/Customers/data/customersItems.ts
+++ src/components/templates/Main/sections/Customers/data/customersItems.ts
@@ -8,61 +8,30 @@ interface ICustomersItem {
   };
 }
 
-export const customersItems: ICustomersItem[] = [
-  {
-    heading: "Unesko",
-    image: {
-      url: "/images/templates/main/customers/logo/unesko.svg",
-      width: 130,
-      height: 51,
-    },
-  },
-  {
-    heading: "Fujitsu",
-    image: {
-      url: "/images/templates/main/customers/logo/fujitsu.svg",
-      width: 92,
-      height: 43,
-    },
-  },
-  {
-    heading: "Croix-Rouge",
-    image: {
-      url: "/images/templates/main/customers/logo/croix-rouge.svg",
-      width: 149,
-      height: 51,
-    },
-  },
-  {
-    heading: "Oracle",
-    image: {
-      url: "/images/templates/main/customers/logo/oracle.svg",
-      width: 117,
-      height: 15,
-    },
-  },
-  {
-    heading: "Suzuki",
-    image: {
-      url: "/images/templates/main/customers/logo/suzuki.svg",
-      width: 70,
-      height: 45,
-    },
-  },
-  {
-    heading: "egress",
-    image: {
-      url: "/images/templates/main/customers/logo/egress.svg",
-      width: 108,
-      height: 27,
-    },
-  },
-  {
-    heading: "Aarnet",
-    image: {
-      url: "/images/templates/main/customers/logo/aarnet.svg",
-      width: 111,
-      height: 38,
-    },
-  },
-];
+interface ICustomersLang {
+  en: ICustomersItem[];
+  fr: ICustomersItem[];
+}
+
+export const customersItems: ICustomersLang = {
+  en: [
+    { heading: "Unesko", image: { url: "/images/templates/main/customers/logo/unesko.svg", width: 130, height: 51 } },
+    { heading: "Fujitsu", image: { url: "/images/templates/main/customers/logo/fujitsu.svg", width: 92, height: 43 } },
+    { heading: "Croix-Rouge", image: { url: "/images/templates/main/customers/logo/croix-rouge.svg", width: 149, height: 51 } },
+    { heading: "Oracle", image: { url: "/images/templates/main/customers/logo/oracle.svg", width: 117, height: 15 } },
+    { heading: "Suzuki", image: { url: "/images/templates/main/customers/logo/suzuki.svg", width: 70, height: 45 } },
+    { heading: "egress", image: { url: "/images/templates/main/customers/logo/egress.svg", width: 108, height: 27 } },
+    { heading: "Aarnet", image: { url: "/images/templates/main/customers/logo/aarnet.svg", width: 111, height: 38 } },
+  ],
+  fr: [
+    { heading: "Unesko", image: { url: "/images/templates/main/customers/logo/unesko.svg", width: 129, height: 56 } },
+    { heading: "Croix-Rouge", image: { url: "/images/templates/main/customers/logo/croix-rouge.svg", width: 149, height: 56 } },
+    { heading: "Infomaniak", image: { url: "/images/templates/main/customers/logo/fr/infomaniak.svg", width: 106, height: 56 } },
+    { heading: "Universite-Paris-Sud", image: { url: "/images/templates/main/customers/logo/fr/universite-paris-sud.svg", width: 102, height: 56 } },
+    { heading: "Ministere-De-I-Interieur", image: { url: "/images/templates/main/customers/logo/fr/ministere-de-i-interieur.svg", width: 107, height: 90 } },
+    { heading: "Suzuki", image: { url: "/images/templates/main/customers/logo/suzuki.svg", width: 69, height: 56 } },
+    { heading: "Oracle", image: { url: "/images/templates/main/customers/logo/oracle.svg", width: 116, height: 56 } },
+  ],
+};
+
+export type TCustomersLocale = keyof typeof customersItems;
