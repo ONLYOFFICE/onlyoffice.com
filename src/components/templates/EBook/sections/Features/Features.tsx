5e01cf755a265b709dc6ab2303a4e1eaadce9911
OlimjonovOqil <okilolimjonov07@gmail.com>
2025-08-15 15:10:15 +0500

    /e-book: add translations and fix styles

--- src/components/templates/EBook/sections/Features/Features.tsx
+++ src/components/templates/EBook/sections/Features/Features.tsx
@@ -1,10 +1,13 @@
-import { useTranslation } from "next-i18next";
+import { Trans, useTranslation } from "next-i18next";
 import { items, tabs } from "./data/items";
 import { EditorsFeatures } from "@src/components/modules/editors/EditorsFeatures";
 import { EditorsTabs } from "@src/components/modules/editors/EditorsTabs";
+import { useRouter } from "next/router";
+import { Link } from "@src/components/ui/Link";
 
 const Features = () => {
   const { t } = useTranslation("e-book");
+  const { locale } = useRouter();
 
   return (
     <>
@@ -14,11 +17,31 @@ const Features = () => {
           ...items,
           id: items.id,
           heading: t(String(items.heading)),
-          text: t(String(items.text)),
-          links: items.links?.map((link) => ({
-            ...link,
-            label: t(String(link.label)),
-          })),
+          text:
+            locale !== "pt" ? (
+              t(String(items.text))
+            ) : (
+              <Trans
+                t={t}
+                i18nKey={String(items.text)}
+                components={[
+                  <Link
+                    href="/app-directory"
+                    color="main"
+                    textUnderline
+                    hover="underline-none"
+                    key="0"
+                  />,
+                ]}
+              />
+            ),
+          links:
+            locale !== "pt"
+              ? items.links?.map((link) => ({
+                  ...link,
+                  label: t(String(link.label)),
+                }))
+              : undefined,
           image: {
             url: t(items.image.url),
             url2x: items.image.url2x && t(items.image.url2x),
