91a51bbb0818f925d59b130c82a8199fe7e10836
Vyacheslav Belov <vyacheslav.belov@onlyoffice.com>
2025-08-25 15:51:21 +0300

    Update sendsubscription.ts

--- src/pages/api/sendsubscription.ts
+++ src/pages/api/sendsubscription.ts
@@ -50,8 +50,6 @@ export default async function handler(
       email: subscribeJson,
     });
 
-    console.log(generateUnsubscribeIdData);
-
     if (
       parsedType === SubscribeType.Common ||
       email === process.env.NONPROFIT_EMAIL
