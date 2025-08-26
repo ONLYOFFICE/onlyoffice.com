c586fede33c025fbd8955aca97fe0b8e27183c96
Amal_Asrarkulov <amal.asrarkulov@onlyoffice.com>
2025-08-26 18:16:34 +0500

    feat: add highlight for Header

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { ILocale } from "@src/types/locale";
 import { Layout } from "@src/components/Layout";
-import { AdventAnnounceBanner } from "@src/components/modules/AdventAnnounceBanner";
+import { AdventAnnounce } from "@src/components/modules/AdventAnnounce";
 import { Header } from "@src/components/modules/Header";
 import { Footer } from "@src/components/modules/Footer";
 import { Head } from "@src/components/modules/head/Head";
@@ -15,15 +15,22 @@ const AboutPage = ({ locale, abouts }: ILocale & IAbouts) => {
   return (
     <Layout>
       <Layout.AdventAnnounce>
-        <AdventAnnounceBanner locale={locale} />
+        <AdventAnnounce locale={locale} />
       </Layout.AdventAnnounce>
       <Layout.Head>
-        <Head title={t("PageTitle")} titleOG={t("PageTitleOG")} description={t("PageDescription")} />
+        <Head
+          title={t("PageTitle")}
+          titleOG={t("PageTitleOG")}
+          description={t("PageDescription")}
+        />
       </Layout.Head>
       <Layout.Header>
         <Header
           locale={locale}
-          highlight={{ buttonId: "oo-menu-item-btn-products" }}
+          highlight={{
+            buttonId: "oo-menu-item-btn-resources",
+            linkId: "oo-menu-link-company"
+          }}
         />
       </Layout.Header>
       <Layout.Main>
