d9d02555961b3657a06bab74405415c048efadf6
Irina Tiulneva <irina.malysheva@onlyoffice.com>
2025-09-03 18:43:03 +0300

    Fix for build (no /DocsRegistration.types). Remove copied parameters.

--- src/pages/docs-registration.tsx
+++ src/pages/docs-registration.tsx
@@ -1,20 +1,15 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { GetServerSidePropsContext } from "next";
-import { parse } from "cookie";
 import { ILocale } from "@src/types/locale";
 import { Layout } from "@src/components/Layout";
 import { Head } from "@src/components/modules/head/Head";
 import { Footer } from "@src/components/modules/Footer";
-import {
-  DocsRegistrationTemplate,
-  IDocsRegistrationTemplate,
-} from "@src/components/templates/DocsRegistration";
+import { DocsRegistrationTemplate } from "@src/components/templates/DocsRegistration";
 
 const DocsRegistrationPage = ({
   locale,
-  queryParams,
-}: ILocale & IDocsRegistrationTemplate) => {
+}: ILocale) => {
   const { t } = useTranslation("docs-registration");
 
   return (
@@ -23,9 +18,7 @@ const DocsRegistrationPage = ({
         <Head title={t("PageTitle")} description={t("PageDescription")} />
       </Layout.Head>
       <Layout.Main>
-        <DocsRegistrationTemplate
-          queryParams={queryParams}
-        />
+        <DocsRegistrationTemplate />
       </Layout.Main>
       <Layout.Footer>
         <Footer locale={locale} />
@@ -36,26 +29,7 @@ const DocsRegistrationPage = ({
 
 export async function getServerSideProps({
   locale,
-  query,
-  req,
 }: GetServerSidePropsContext & ILocale) {
-  const {
-    language,
-    spam,
-    partnerId,
-    affiliateId,
-  } = query;
-
-  const cookies = parse(req.headers.cookie || "");
-  const utmCampaign = cookies.utm_campaign || null;
-
-  const queryString = new URLSearchParams();
-  if (language) queryString.append("language", String(language));
-  if (spam) queryString.append("spam", String(spam));
-  if (partnerId) queryString.append("partnerId", String(partnerId));
-  if (affiliateId) queryString.append("affiliateId", String(affiliateId));
-  if (utmCampaign) queryString.append("campaign", String(utmCampaign));
-  const queryParams = queryString.toString();
 
   return {
     props: {
@@ -65,7 +39,6 @@ export async function getServerSideProps({
         "Select",
       ])),
       locale,
-      queryParams,
     },
   };
 }
