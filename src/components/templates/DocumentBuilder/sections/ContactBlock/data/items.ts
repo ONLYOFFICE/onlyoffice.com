0d0a98a76223bc334084371cd653b19c02f2ce16
OlimjonovOqil <Okilolimjonov07@gmail.com>
2025-09-05 18:45:23 +0500

    fix /sr translations

--- src/components/templates/DocumentBuilder/sections/ContactBlock/data/items.ts
+++ src/components/templates/DocumentBuilder/sections/ContactBlock/data/items.ts
@@ -1,10 +1,13 @@
 import { IContactBlock } from "../ContactBlock.types";
 
 export const items: IContactBlock = {
-    heading: "ONLYOFFICEDocumentBuilderPart",
-    text: [
-        { label: "BringAdvancedDocument", url: "https://teamlab.info/developer-edition.aspx", target: "_blank" },
-        { label: "HaveAnyQuestions" },
-    ],
-    button: { label: "GetInTouch", url: "mailto:sales@onlyoffice.com?subject=ONLYOFFICE Document Builder request"}
-}
+  heading: "ONLYOFFICEDocumentBuilderPart",
+  text: [
+    { label: "BringAdvancedDocument", url: "/developer-edition" },
+    { label: "HaveAnyQuestions" },
+  ],
+  button: {
+    label: "GetInTouch",
+    url: "mailto:sales@onlyoffice.com?subject=ONLYOFFICE%20Document%20Builder%20request",
+  },
+};
