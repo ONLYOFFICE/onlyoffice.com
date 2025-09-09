2cd817bd54753a53ac9011e28320a5e3c1a335e0
OlimjonovOqil <Okilolimjonov07@gmail.com>
2025-09-09 14:45:04 +0500

    add request to SubscriptionModal

--- src/components/modules/SocialIcons/sub-components/SubscribeModal/SubscribeModal.tsx
+++ src/components/modules/SocialIcons/sub-components/SubscribeModal/SubscribeModal.tsx
@@ -17,9 +17,12 @@ import {
 import { Modal } from "@src/components/ui/Modal";
 import { CrossIcon } from "@src/components/icons";
 import { validateEmail } from "@src/utils/validators";
+import { useRouter } from "next/router";
+import { ISubscribeInput } from "@src/components/widgets/SubscribeInput";
 
 const SubscribeModal = ({ isOpen, onClose }: IMailModal) => {
   const { t } = useTranslation("SubscribeModal");
+  const { locale } = useRouter();
 
   const [formData, setFormData] = useState({
     name: "",
@@ -30,6 +33,9 @@ const SubscribeModal = ({ isOpen, onClose }: IMailModal) => {
     email: false,
   });
   const [isFormValid, setIsFormValid] = useState(false);
+  const [subscribeStatus, setSubscribeStatus] =
+    useState<ISubscribeInput["status"]>("default");
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const handleInputChange = (field: string, value: string) => {
     setFormData((prevData) => ({
@@ -49,7 +55,7 @@ const SubscribeModal = ({ isOpen, onClose }: IMailModal) => {
     setIsFormValid(nameIsValid && emailIsValid);
   };
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     const nameIsEmpty = formData.name.trim().length === 0;
     const emailIsEmpty = formData.email.trim().length === 0;
     const emailIsValid = validateEmail(formData.email);
@@ -62,7 +68,35 @@ const SubscribeModal = ({ isOpen, onClose }: IMailModal) => {
     const isValid = !nameIsEmpty && !emailIsEmpty && emailIsValid;
     setIsFormValid(isValid);
 
-    if (!isValid) return;
+    if (!isValid || !isFormValid) return;
+
+    setSubscribeStatus("loading");
+
+    try {
+      const res = await fetch("/api/sendsubscription", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          language: locale === "en" ? "" : locale,
+          firstName: formData.name,
+          email: formData.email,
+          type: "Common",
+        }),
+      });
+
+      const data = await res.json();
+
+      if (data.status === "success") {
+        setIsSuccess(true);
+        setSubscribeStatus("success");
+      } else {
+        console.log(data.message);
+        setSubscribeStatus("error");
+      }
+    } catch {
+      setIsSuccess(false);
+      setSubscribeStatus("error");
+    }
   };
 
   const onCloseModal = () => {
@@ -76,6 +110,8 @@ const SubscribeModal = ({ isOpen, onClose }: IMailModal) => {
       email: false,
     });
     setIsFormValid(false);
+    setSubscribeStatus("default");
+    setIsSuccess(false);
   };
 
   return (
@@ -95,7 +131,7 @@ const SubscribeModal = ({ isOpen, onClose }: IMailModal) => {
             </StyledMailModalCloseBtn>
           </StyledMailModalHeader>
 
-          {isFormValid ? (
+          {isSuccess ? (
             <>
               <StyledMailModalConfirmText
                 color="#666666"
@@ -160,6 +196,7 @@ const SubscribeModal = ({ isOpen, onClose }: IMailModal) => {
                 <StyledMailModalBtnWrapper>
                   <StyledMailModalBtn
                     onClick={onSubmit}
+                    disabled={subscribeStatus === "loading"}
                     label={t("Subscribe")}
                   />
                 </StyledMailModalBtnWrapper>
