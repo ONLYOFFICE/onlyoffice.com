ad1984915e9e56d97d3ee074617c13e4ea614cd3
samirkerimov <84935773+Sam-oops@users.noreply.github.com>
2025-09-17 15:24:53 +0500

    /best-hancom-alternative: Update text in "Ai helper" block and add link

--- src/components/templates/BestHancomAlternative/sections/Reasons/data/items.ts
+++ src/components/templates/BestHancomAlternative/sections/Reasons/data/items.ts
@@ -1,47 +1,57 @@
 import { IFeatureItem } from "@src/components/widgets/FeatureItem";
 
-export const items: IFeatureItem[] = [
+interface IFeatureItemExtended extends Omit<IFeatureItem, "text"> {
+  text: {
+    label: React.ReactNode;
+    links?: { href: string; isExternal?: boolean }[];
+  };
+}
+
+export const items: IFeatureItemExtended[] = [
   {
     icon: {
       url: "/images/templates/best-hancom-alternative/reasons/open_source.svg",
     },
     heading: "OpenSource",
-    text: "OpenSourceText",
+    text: { label: "OpenSourceText" },
   },
   {
     icon: {
       url: "/images/templates/best-hancom-alternative/reasons/desktop_app.svg",
     },
     heading: "DesktopApp",
-    text: "DesktopAppText",
+    text: { label: "DesktopAppText" },
   },
   {
     icon: {
       url: "/images/templates/best-hancom-alternative/reasons/docs_sheets_slides.svg",
     },
     heading: "DocsSheetsSlides",
-    text: "DocsSheetsSlidesText",
+    text: { label: "DocsSheetsSlidesText" },
   },
   {
     icon: {
       url: "/images/templates/best-hancom-alternative/reasons/security.svg",
     },
     heading: "Security",
-    text: "SecurityText",
+    text: { label: "SecurityText" },
   },
   {
     icon: {
       url: "/images/templates/best-hancom-alternative/reasons/ai_helper.svg",
     },
     heading: "AIHelper",
-    text: "AIHelperText",
+    text: { 
+      label: "AIHelperText",
+      links: [{ href: "/ai-assistants" }],
+    },
   },
   {
     icon: {
       url: "/images/templates/best-hancom-alternative/reasons/flexibility.svg",
     },
     heading: "Flexibility",
-    text: "FlexibilityText",
+    text: { label: "FlexibilityText" },
   },
   {
     icon: {
@@ -49,6 +59,6 @@ export const items: IFeatureItem[] = [
     },
     variant: "vertical",
     heading: "Seamless",
-    text: "SeamlessText",
+    text: { label: "SeamlessText" },
   },
 ];
