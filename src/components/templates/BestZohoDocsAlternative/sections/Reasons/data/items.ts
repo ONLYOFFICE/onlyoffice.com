ffadc2e210a6b49e197f9010189e7cd6ddffe1b9
samirkerimov <84935773+Sam-oops@users.noreply.github.com>
2025-09-17 12:46:42 +0500

    /best-zoho-docs-alternative: Update text in "Ai helper" block

--- src/components/templates/BestZohoDocsAlternative/sections/Reasons/data/items.ts
+++ src/components/templates/BestZohoDocsAlternative/sections/Reasons/data/items.ts
@@ -1,54 +1,64 @@
 import { IFeatureItem } from "@src/components/widgets/FeatureItem";
 
-export const items: IFeatureItem[] = [
+interface IFeatureItemExtended extends Omit<IFeatureItem, "text"> {
+  text: {
+    label: React.ReactNode;
+    links?: { href: string; isExternal?: boolean }[];
+  };
+}
+
+export const items: IFeatureItemExtended[] = [
   {
     icon: {
       url: "/images/templates/best-zoho-docs-alternative/reasons/open_source.svg",
     },
     heading: "OpenSource",
-    text: "OpenSourceText",
+    text: { label: "OpenSourceText" },
   },
   {
     icon: {
       url: "/images/templates/best-zoho-docs-alternative/reasons/desktop_app.svg",
     },
     heading: "DesktopApp",
-    text: "DesktopAppText",
+    text: { label: "DesktopAppText" },
   },
   {
     icon: {
       url: "/images/templates/best-zoho-docs-alternative/reasons/docs_sheets_slides.svg",
     },
     heading: "DocsSheetsSlides",
-    text: "DocsSheetsSlidesText",
+    text: { label: "DocsSheetsSlidesText" },
   },
   {
     icon: {
       url: "/images/templates/best-zoho-docs-alternative/reasons/seamless.svg",
     },
     heading: "Seamless",
-    text: "SeamlessText",
+    text: { label: "SeamlessText" },
   },
   {
     icon: {
       url: "/images/templates/best-zoho-docs-alternative/reasons/flexibility.svg",
     },
     heading: "Flexibility",
-    text: "FlexibilityText",
+    text: { label: "FlexibilityText" },
   },
   {
     icon: {
       url: "/images/templates/best-zoho-docs-alternative/reasons/ai_helper.svg",
     },
     heading: "AIHelper",
-    text: "AIHelperText",
+    text: {
+      label: "AIHelperText",
+      links: [{ href: "/ai-assistants" }],
+    },
   },
   {
     icon: {
       url: "/images/templates/best-zoho-docs-alternative/reasons/security.svg",
     },
     heading: "Security",
-    text: "SecurityText",
+    text: { label: "SecurityText" },
     variant: "vertical",
   },
 ];
